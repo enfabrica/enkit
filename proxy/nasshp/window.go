4be1781f90339d897c390a2c10446b4fc3a63019
Carlo Contavalli <ccontavalli@gmail.com>
2020-09-02 09:38:32 -0700

    Basic window primitives working.

--- proxy/nasshp/window.go
+++ proxy/nasshp/window.go
@@ -2,115 +2,75 @@ package nasshp
 
 import (
 	"fmt"
-	"log"
 	"sync"
 )
 
-type SendWindow struct {
-	buffer []byte
+type buffer struct {
+	// We use the slice to:
+	// - maintain a pointer to the start of the data.
+	// - know the amount of memory allocated - cap(data).
+	// - know the amount of memory actually used - len(data).
+	data []byte
+	// Offset indicates how many bytes at the beginning of data to skip.
+	// This way, we don't have to update the data pointer.
+	offset int
+	// Acknowledged indicates how many bytes have been acknowledged.
+	acknowledged int
 
-	ackstart, fillstart, emptystart int
-	acknowledged, filled, emptied   uint64
+	// Used to maintain a chain of buffers.
+	prev, next *buffer
 }
 
-func NewSendWindow(bsize int) *SendWindow {
-	return &SendWindow{
-		buffer: make([]byte, bsize),
-	}
+type blist buffer
+
+func (bl *blist) Init() *blist {
+	bl.next = (*buffer)(bl)
+	bl.prev = (*buffer)(bl)
+	return bl
 }
 
-func (w *SendWindow) ToFill() []byte {
-	if w.ackstart <= w.fillstart {
-		if w.ackstart == 0 {
-			return w.buffer[w.fillstart : len(w.buffer)-1]
-		}
-		return w.buffer[w.fillstart:]
-	}
-	return w.buffer[w.fillstart : w.ackstart-1]
+func (bl *blist) First() *buffer {
+	return bl.next
 }
 
-func (w *SendWindow) Filled(size int) {
-	w.filled += uint64(size)
-
-	w.fillstart += size
-	if w.fillstart == len(w.buffer) {
-		w.fillstart = 0
-	}
+func (bl *blist) Last() *buffer {
+	return bl.prev
 }
 
-func (w *SendWindow) ToEmpty() []byte {
-	if w.emptystart > w.fillstart {
-		return w.buffer[w.emptystart:]
-	}
-	return w.buffer[w.emptystart:w.fillstart]
+func (bl *blist) InsertListBefore(where *buffer, list *blist) {
+	first := list.First()
+	where.prev.next = first
+	first.prev = where.prev
+
+	last := list.Last()
+	where.prev = last
+	last.next = where
+
+	// Make sure there are no lingering pointers in the original list.
+	list.Init()
 }
 
-func (w *SendWindow) Empty(size int) {
-	w.emptied += uint64(size)
+func (bl *blist) InsertAfter(where, what *buffer) *buffer {
+	what.prev = where
+	what.next = where.next
 
-	w.emptystart += size
-	if w.emptystart == len(w.buffer) {
-		w.emptystart = 0
-	}
+	where.next.prev = what
+	where.next = what
+	return what
 }
 
-func (w *SendWindow) AcknowledgeUntil(trunc uint32) error {
-	until := ToAbsolute(w.acknowledged, trunc)
-	if until < w.acknowledged || until > w.emptied {
-		return fmt.Errorf("ack is acknowledging more than allowed")
-	}
-
-	diff := w.acknowledged - until
-	w.acknowledged = until
-
-	w.ackstart += int(diff)
-	if w.ackstart > len(w.buffer) {
-		w.ackstart -= len(w.buffer)
-	}
-	return nil
+func (bl *blist) Append(toadd *buffer) *buffer {
+	res := bl.InsertAfter(bl.Last(), toadd)
+	return res
 }
 
-// Examples:
-// - let's say we only send the last digit, 0 to 9
-// - current value is 13.
-//   - We get 4. Likely means 14.
-//   - We get 9. 19? or 9?
-// - current value is 19.
-//   - We get 8. Does it mean 18? or does it mean 28?
-func ToAbsolute(reference uint64, trunc uint32) uint64 {
-	base := uint32(reference & 0xffffff)
-	diff := uint32(0)
-	if trunc >= base {
-		diff = trunc - base
-	} else {
-		diff = 0x1000000 - (base - trunc + 1)
-	}
-
-	topbits := reference & ^uint64(0xffffff)
-	switch {
-	case diff > 0x7fffff && trunc > base && topbits > 0x1000000:
-		return (topbits - 0x1000000) | uint64(trunc)
-	case diff <= 0x7fffff && trunc < base:
-		return (topbits + 0x1000000) | uint64(trunc)
-	}
-	return topbits | uint64(trunc)
+func (bl *blist) Drop(todrop *buffer) *buffer {
+	todrop.prev.next, todrop.next.prev = todrop.next, todrop.prev
+	return todrop
 }
 
-func (w *SendWindow) Reset(trunc uint32) error {
-	from := ToAbsolute(w.acknowledged, trunc)
-	if from < w.acknowledged || from > w.filled {
-		return fmt.Errorf("request ack reset outside allowed range")
-	}
-
-	diff := w.acknowledged - from
-	w.acknowledged = from
-	w.ackstart += int(diff)
-	if w.ackstart > len(w.buffer) {
-		w.ackstart -= len(w.buffer)
-	}
-	w.emptystart = w.ackstart
-
-	return nil
+func (bl *blist) End() *buffer {
+	return (*buffer)(bl)
 }
 
 type BufferPool sync.Pool
@@ -131,82 +91,157 @@ func (bp *BufferPool) Get() *buffer {
 }
 
 func (bp *BufferPool) Put(b *buffer) {
-	// Reset buffer.
+	// Ensure that when the buffer is recycled, it is empty.
 	b.data = b.data[:0]
-	// Zero other fields.
-	b.emptied = 0
+	b.offset = 0
+	b.acknowledged = 0
+	// This is pretty much only for defense in depth.
+	b.prev, b.next = nil, nil
 
 	sp := (*sync.Pool)(bp)
 	sp.Put(b)
 }
 
-type buffer struct {
-	data    []byte
-	emptied int
+type SendWindow struct {
+	filled       uint64 // Absolute counter of bytes filled.
+	acknowledged uint64 // Absolute counter of bytes acknowledged from this window.
+	emptied      uint64 // Absolute counter of bytes consumed from this window.
 
-	prev, next *buffer
+	pool    *BufferPool
+	buffer  blist
+	pending blist
 }
 
-type blist buffer
+func NewSendWindow(pool *BufferPool) *SendWindow {
+	w := &SendWindow{pool: pool}
 
-func (bl *blist) Init() {
-	bl.next = (*buffer)(bl)
-	bl.prev = (*buffer)(bl)
+	w.buffer.Init()
+	w.pending.Init()
+	return w
 }
 
-func (bl *blist) First() *buffer {
-	return bl.next
+func (w *SendWindow) ToFill() []byte {
+	last := w.buffer.Last()
+	if cap(last.data) > len(last.data) {
+		return last.data[len(last.data):cap(last.data)]
+	}
+
+	last = w.buffer.Append(w.pool.Get())
+	return last.data[:cap(last.data)]
 }
 
-func (bl *blist) Last() *buffer {
-	//log.Printf("last %p", bl.prev)
-	return bl.prev
+func (w *SendWindow) Filled(size int) {
+	last := w.buffer.Last()
+
+	w.filled += uint64(size)
+	last.data = last.data[0 : len(last.data)+size]
 }
 
-// End return the buffer pointer used to indicate the end of the chain.
-func (bl *blist) End() *buffer {
-	return (*buffer)(bl)
+func (w *SendWindow) ToEmpty() []byte {
+	first := w.buffer.First()
+	return first.data[first.offset:]
 }
 
-// blist.next = b1
-// blist.prev = b1
-//
-// b1.next = blist
-// b1.prev = blist
-//
-// InsertAfter(b1, n)
-//
-// n.prev = b1
-// n.next = b1.next = blist
-// b1.next.prev = n --> blist.prev = n
-// b1.next = n
-//
-// blist.next =b1
-// b1.next = n
-// n.next = blist
+func (w *SendWindow) Empty(size int) {
+	w.emptied += uint64(size)
+	for size > 0 {
+		first := w.buffer.First()
+		filled := first.offset + size
+		if filled < len(first.data) {
+			first.offset += size
+			break
+		}
 
-// blist.prev = n
-// n.prev = b1
-// b1.prev = blist
-func (bl *blist) InsertAfter(where, what *buffer) *buffer {
-	what.prev = where
-	what.next = where.next
+		size -= len(first.data) - first.offset
 
-	where.next.prev = what
-	where.next = what
-	return what
+		// Offset = 0 is important if the ack # is reset, and we have to go back in time.
+		first.offset = 0
+		w.pending.Append(w.buffer.Drop(first))
+	}
 }
 
-func (bl *blist) Append(toadd *buffer) *buffer {
-	res := bl.InsertAfter(bl.Last(), toadd)
-	//log.Printf("appended %p, last %p", toadd, bl.Last())
-	return res
+func (w *SendWindow) Acknowledge(size int) {
+	w.acknowledged += uint64(size)
+
+	// For anything in pending, we can ignore offset, as the buffer has already been processed.
+	for {
+		if size <= 0 {
+			return
+		}
+
+		first := w.pending.First()
+		if first == w.pending.End() {
+			break
+		}
+
+		acked := first.acknowledged + size
+		if acked < len(first.data) {
+			first.acknowledged += size
+			return
+		}
+
+		size -= len(first.data) - first.acknowledged
+		w.pool.Put(w.pending.Drop(first))
+	}
+
+	// If we are here, we reached the end of pending buffers, and still have data to acknowledge.
+
+	first := w.buffer.First()
+	if first.acknowledged+size < first.offset {
+		first.acknowledged += size
+		return
+	}
+
+	first.acknowledged = first.offset
 }
 
-func (bl *blist) Drop(todrop *buffer) *buffer {
-	todrop.prev.next, todrop.next.prev = todrop.next, todrop.prev
-	todrop.prev, todrop.next = nil, nil // Strictly not necessary, for defense in depth.
-	return todrop
+func (w *SendWindow) AcknowledgeUntil(trunc uint32) error {
+	value := ToAbsolute(w.acknowledged, trunc)
+	if value > w.emptied {
+		return fmt.Errorf("impossible ack: acknowledges data that was never sent")
+	}
+	if value < w.acknowledged {
+		return fmt.Errorf("political ack: the data had been acked already, but this ack claims it never was - and now wants to resume from data we no longer have")
+	}
+	w.Acknowledge(int(value - w.acknowledged))
+	return nil
+}
+
+func (w *SendWindow) Reset(trunc uint32) error {
+	value := ToAbsolute(w.acknowledged, trunc)
+	if value > w.emptied {
+		return fmt.Errorf("invalid ack, puts us past send buffer")
+	}
+	if value < w.acknowledged {
+		return fmt.Errorf("invalid ack, requires going before last ack, for which we have no buffer")
+	}
+
+	w.Acknowledge(int(value - w.acknowledged))
+	w.buffer.InsertListBefore(w.buffer.First(), &w.pending)
+
+	w.buffer.First().offset = w.buffer.First().acknowledged
+	w.emptied = w.acknowledged
+	w.filled = w.acknowledged
+	return nil
+}
+
+func ToAbsolute(reference uint64, trunc uint32) uint64 {
+	base := uint32(reference & 0xffffff)
+	diff := uint32(0)
+	if trunc >= base {
+		diff = trunc - base
+	} else {
+		diff = 0x1000000 - (base - trunc + 1)
+	}
+
+	topbits := reference & ^uint64(0xffffff)
+	switch {
+	case diff > 0x7fffff && trunc > base && topbits > 0x1000000:
+		return (topbits - 0x1000000) | uint64(trunc)
+	case diff <= 0x7fffff && trunc < base:
+		return (topbits + 0x1000000) | uint64(trunc)
+	}
+	return topbits | uint64(trunc)
 }
 
 type ReceiveWindow struct {
@@ -255,13 +290,10 @@ func (w *ReceiveWindow) Reset(wack uint32) error {
 func (w *ReceiveWindow) ToFill() []byte {
 	last := w.buffer.Last()
 	if cap(last.data) > len(last.data) {
-		log.Printf("existing")
 		return last.data[len(last.data):cap(last.data)]
 	}
 
-	log.Printf("new - %p %d %d", last, cap(last.data), len(last.data))
 	last = w.buffer.Append(w.pool.Get())
-	log.Printf("created - %p %d %d", last, cap(last.data), len(last.data))
 	return last.data[:cap(last.data)]
 }
 
@@ -269,15 +301,13 @@ func (w *ReceiveWindow) Filled(size int) {
 	last := w.buffer.Last()
 
 	// If we already emptied this data, we need to discard it.
-	log.Printf("%p filled %d emptied %d reset %d", last, w.filled, w.emptied, w.reset)
 	if w.reset != 0 && w.reset < w.filled {
 		sentalready := w.filled - w.reset
 		if sentalready > uint64(size) {
 			w.reset += uint64(size)
 			return
 		}
-		log.Printf("sentalready %d", int(sentalready))
-		last.emptied = int(sentalready) // Skip the first sentalready bytes of the buffer when ToEmpty is called.
+		last.offset = int(sentalready) // Skip the first sentalready bytes of the buffer when ToEmpty is called.
 		w.reset = 0
 
 		w.filled += uint64(size) - sentalready
@@ -290,17 +320,16 @@ func (w *ReceiveWindow) Filled(size int) {
 
 func (w *ReceiveWindow) ToEmpty() []byte {
 	first := w.buffer.First()
-	return first.data[first.emptied:]
+	return first.data[first.offset:]
 }
 
 func (w *ReceiveWindow) Empty(size int) {
 	w.emptied += uint64(size)
-	log.Printf("incremented emptied by %d, emp %d", size, w.emptied)
 	for size > 0 {
 		first := w.buffer.First()
-		filled := first.emptied + size
+		filled := first.offset + size
 		if filled < len(first.data) {
-			first.emptied += size
+			first.offset += size
 			break
 		}
 
@@ -311,13 +340,12 @@ func (w *ReceiveWindow) Empty(size int) {
 		// application, with small writes, where the reader catches up immediately,
 		// it allows to re-use the same buffer over and over.
 		if first == w.buffer.Last() {
-			first.emptied = 0
+			first.offset = 0
 			first.data = first.data[:0]
 			break
 		}
 
-		size -= len(first.data) - first.emptied
-		log.Printf("dropping buffer - e %d l %d", first.emptied, len(first.data))
+		size -= len(first.data) - first.offset
 		w.pool.Put(w.buffer.Drop(first))
 	}
 }
--- proxy/nasshp/window_test.go
+++ proxy/nasshp/window_test.go
@@ -2,21 +2,88 @@ package nasshp
 
 import (
 	"github.com/stretchr/testify/assert"
-	"log"
+	//	"log"
 	"testing"
 )
 
-var quote = "There are no facts, only interpretations." // 41 bytes long.
+// This quote is 41 bytes long. Very convenient, as 41 is prime, and won't align with powers of 2.
+var quote = "There are no facts, only interpretations."
+
+func TestSendWindowSimpleAcknowledge(t *testing.T) {
+	bsize := 32 // Small buffer size to trigger as many buffer transition as possible.
+	p := NewBufferPool(bsize)
+	w := NewSendWindow(p)
+
+	err := w.AcknowledgeUntil(0)
+	assert.Nil(t, err)
+
+	err = w.AcknowledgeUntil(2)
+	assert.NotNil(t, err)
+
+	d := w.ToEmpty()
+	assert.Equal(t, 0, len(d))
+
+	d = w.ToFill()
+	l := copy(d, quote)
+	assert.Equal(t, bsize, l)
+	w.Filled(l)
+
+	// Still failing, as the data has not been sent.
+	err = w.AcknowledgeUntil(2)
+	assert.NotNil(t, err)
+
+	// Mark 4 bytes as sent.
+	d = w.ToEmpty()
+	assert.Equal(t, bsize, len(d))
+	w.Empty(4)
+	d = w.ToEmpty()
+	assert.Equal(t, bsize-4, len(d))
+
+	// Success!
+	err = w.AcknowledgeUntil(2)
+	assert.Nil(t, err)
+
+	// Shouldn't change how much data we have left to empty.
+	d = w.ToEmpty()
+	assert.Equal(t, bsize-4, len(d))
+
+	// No change if we re-acknowledge the same data.
+	err = w.AcknowledgeUntil(2)
+	assert.Nil(t, err)
+	// Error if we go back and un-acknowledge some data.
+	err = w.AcknowledgeUntil(1)
+	assert.NotNil(t, err)
+
+	// Same amount of data to empty.
+	d = w.ToEmpty()
+	assert.Equal(t, bsize-4, len(d))
+
+	// Acknowledge up to all the data we have.
+	err = w.AcknowledgeUntil(4)
+	assert.Nil(t, err)
+	d = w.ToEmpty()
+	assert.Equal(t, bsize-4, len(d))
+
+	// Finally, acknowledge everything.
+	w.Empty(len(d))
+	err = w.AcknowledgeUntil(uint32(bsize))
+	assert.Nil(t, err)
+
+	d = w.ToEmpty()
+	assert.Equal(t, 0, len(d))
+	d = w.ToFill()
+	assert.Equal(t, bsize, len(d))
+}
 
 func TestToAbsolute(t *testing.T) {
-	log.Printf("simple batch")
+	t.Logf("simple batch")
 	for i := 0; i < 12; i++ {
 		assert.Equal(t, uint64(i+0x7ffffa), ToAbsolute(uint64(0), uint32(i+0x7ffffa)))
 	}
 
 	// Positive delta: the last 24 bits exceed the current 24 bits.
 	// Numbers have been picked so in 6 iterations the delta exceeds 2**23
-	log.Printf("positive delta")
+	t.Logf("positive delta")
 	for i := 0; i < 12; i++ {
 		if i < 6 {
 			assert.Equal(t, uint64(i+0x107ffffa), ToAbsolute(uint64(0x10000000), uint32(i+0x7ffffa)))
@@ -26,13 +93,109 @@ func TestToAbsolute(t *testing.T) {
 	}
 
 	// Negative delta: the last 24 bits lare less than the current 24 bits.
-	log.Printf("negative delta")
+	t.Logf("negative delta")
 	for i := 0; i < 12; i++ {
 		if i <= 6 {
 			assert.Equal(t, uint64(0x10000000+12-i), ToAbsolute(uint64(0x107fffff+6), uint32(12-i)))
 		} else {
 			assert.Equal(t, uint64(0x11000000+12-i), ToAbsolute(uint64(0x107fffff+6), uint32(12-i)))
 		}
+	}
+}
+
+func TestSendWindowSimple(t *testing.T) {
+	qb := []byte{}
+	for i := 0; i < 1000; i++ {
+		qb = append(qb, []byte(quote)...)
+	}
+
+	bsize := 32 // Small buffer size to trigger as many buffer transition as possible.
+	p := NewBufferPool(bsize)
+	w := NewSendWindow(p)
+
+	// Fill the entire buffer with quotes.
+	loops := 0
+	for copied := 0; copied < len(qb); loops++ {
+		d := w.ToFill()
+		assert.True(t, len(d) > 0, "should never return a 0 sized buffer")
+
+		c := copy(d, qb[copied:])
+		w.Filled(c)
+		copied += c
+	}
+	assert.Equal(t, len(qb)/32+1, loops)
+
+	// Verify the entire buffer.
+	loops = 0
+	for verified := 0; verified < len(qb); loops++ {
+		d := w.ToEmpty()
+		assert.True(t, len(d) > 0, "should never return a 0 sized buffer")
+		assert.Equal(t, string(qb[verified:verified+len(d)]), string(d), "diff in loop %d, offset %d", loops, verified)
+
+		// Ensure that we empty the buffer at different steps / sizes.
+		step := (loops % len(d)) + 1
+		j := 0
+		for {
+			if j+step > len(d) {
+				w.Empty(len(d) - j)
+				break
+			}
+
+			w.Empty(step)
+			j += step
+		}
+		verified += len(d)
+	}
+	assert.Equal(t, len(qb)/32+1, loops)
+
+	// Acknowledge roughly half the data.
+	verified := 0
+	for verified < len(qb)/2 {
+		w.Acknowledge(17) // not a multiple of 32, or 41, should challenge different offsets.
+
+		d := w.ToEmpty()
+		assert.Equal(t, 0, len(d))
+		e := w.ToFill()
+		assert.Equal(t, 32, len(e))
+
+		verified += 17
+	}
+
+	// Try to reset the acknowledge number.
+	err := w.Reset(12)
+	assert.NotNil(t, err, "trying to seek back to an already acknowledged byte")
+	err = w.Reset(uint32(len(qb) + 12))
+	assert.NotNil(t, err, "trying to seek after data in buffer")
+
+	// Reset the state of the buffer, we should have seeked back to the beginning.
+	for i := 0; ; i++ {
+		verified += 17
+		if verified >= len(qb) {
+			break
+		}
+		err = w.Reset(uint32(verified))
+		assert.Nil(t, err)
+		if err != nil {
+			t.Fail()
+			break
+		}
+
+		inner := verified
+		for loops = 0; inner < len(qb); loops++ {
+			d := w.ToEmpty()
+			assert.True(t, len(d) > 0, "should never return a 0 sized buffer")
+			if !assert.Equal(t, string(qb[inner:inner+len(d)]), string(d), "verified %d diff in loop %d, offset %d", verified, loops, inner) {
+				break
+			}
+			w.Empty(len(d))
+			if len(d) == 0 {
+				t.Fail()
+				break
+			}
+
+			inner += len(d)
+		}
+		t.Logf("TOTAL LOOPS %d - verified %d out of %d", loops, verified, len(qb))
 	}
 }
 
@@ -135,18 +298,15 @@ func TestReceiveWindowReset(t *testing.T) {
 	w.Filled(5)
 
 	// Let's consume the buffer.
-	log.Printf("consuming")
 	data = w.ToEmpty()
 	assert.Equal(t, quote, string(data))
 	w.Empty(len(data))
-	log.Printf("consumed")
 
 	data = w.ToEmpty()
 	assert.Equal(t, quote[3:5], string(data))
 	w.Empty(len(data))
 
 	data = w.ToFill()
-	log.Printf("filling more %d", len(data))
 	copy(data, quote)
 	w.Filled(len(quote))
 
