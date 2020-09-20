e4a2625cf44c4a3501de20335c3b4650c597a971
Carlo Contavalli <ccontavalli@gmail.com>
2020-09-20 13:41:40 -0700

    doc: add documentation to HostDispatch object.
    Before this change, the HostDispatch library had no documentation.
    
    With this change, added basic docstrings explaining how to use the
    library, and the meaning of the functions.

--- lib/khttp/dispatch.go
+++ lib/khttp/dispatch.go
@@ -8,6 +8,16 @@ import (
 	"strings"
 )
 
+// HostDispatcher is an http.Handler (implements ServeHTTP) that invokes
+// a different http.Handler based on the host specified in the HTTP request.
+//
+// It complements http.ServeMUX in the sense that http.ServeMUX invokes different
+// http.Handler based on the path of the request, while HostDispatcher invokes
+// them based on the host header.
+//
+// By combining the two, you can easily create handlers with multiple virtual
+// hosts involved.
+//
 type HostDispatcher map[string]http.Handler
 
 func (hd HostDispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -27,11 +37,29 @@ func (hd HostDispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, fmt.Sprintf("Host '%s' not found", r.Host), http.StatusNotFound)
 }
 
+// HostDispatch mapping. Maps a string to a http handler.
+//
+// If the host is empty, the corresponding http.Handler will be invoked when the
+// host is unknown, as well as for every host that has not been configured explicitly.
 type HostDispatch struct {
+	// Which host the client has to request in the Host HTTP header for the Handler
+	// below to be invoked. If empty, the handler will be invoked for every request
+	// for which a direct match cannot be found.
+	//
+	// Matching is case insensitive. Domains with a period appended are considered
+	// equivalent to domains without a period. Example: www.mydomain.com is considered
+	// equivalent to www.mydomain.com. (trailing period).
+	//
+	// The Host string can also specify a port number for example: www.mydomain.com:1001.
+	// Port 80 or 443 are stripped by default and matched without port, as the RFC
+	// recommandation is that port 80 and 443 are to be stripped in host headers.
 	Host    string
+
+	// Handler is the handler to invoke for all requests matching this host.
 	Handler http.Handler
 }
 
+// NewHostDispatcher creates a new HostDispatcher.
 func NewHostDispatcher(todispatch []HostDispatch) (HostDispatcher, error) {
 	hd := HostDispatcher{}
 	var errs []error
