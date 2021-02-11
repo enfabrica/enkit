e8b8e9a335666564e2029320dbd7363cd575d0f5
Stefano Duo <stefano@enfabrica.net>
2021-02-11 23:32:48 +0100

    kernel_test: Add default name for repo
    Use the expected default name @enkit as the repo_name parameter of
    the kernel_test macro. Add the tap parser as an additional parameter of
    the macro.

--- bazel/linux/run_um_kunit_tests.sh
+++ bazel/linux/run_um_kunit_tests.sh
@@ -5,10 +5,10 @@ set -e
 LINUXIMG=$1
 ROOTFS=$2
 HOSTFS=$(dirname $(realpath $3))
+KUNIT_TAP_PARSER=$4
 
 export TMPDIR=$(mktemp -d)
 OUTFILE="${TMPDIR}/output"
-KUNIT_TAP_PARSER=/path/to/kunit_tap_parser
 
 "$LINUXIMG" ubd0="$ROOTFS" hostfs="$HOSTFS" | tee "$OUTFILE"
 "$KUNIT_TAP_PARSER" parse < "$OUTFILE"
