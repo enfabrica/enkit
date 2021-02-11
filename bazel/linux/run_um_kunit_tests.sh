9899c9707b577dc128fbd65122828aa945182748
Stefano Duo <stefano@enfabrica.net>
2021-02-11 17:49:18 +0100

    run_um_kunit_tests.sh: use mktemp to init TMPDIR
    Instead of rolling our own buggy solution to create a temporary dir use
    the builtin function mktemp.

--- bazel/linux/run_um_kunit_tests.sh
+++ bazel/linux/run_um_kunit_tests.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
+set -e
+
 LINUXIMG=$1
 ROOTFS=$2
 HOSTFS=$(dirname $(realpath $3))
 
-set -e
-
-mkdir -p /tmp/uml
-USER=$(whoami)
-chown "${USER}.${USER}" /tmp/uml
-chmod 777 /tmp/uml
-export TMPDIR=/tmp/uml
-
+export TMPDIR=$(mktemp -d)
 OUTFILE="${TMPDIR}/output"
 KUNIT_TAP_PARSER=/path/to/kunit_tap_parser
 
-"$LINUXIMG" ubd0="$ROOTFS" hostfs="$HOSTFS" | tee $OUTFILE
-KUNIT_TAP_PARSER parse < $OUTFILE
+"$LINUXIMG" ubd0="$ROOTFS" hostfs="$HOSTFS" | tee "$OUTFILE"
+"$KUNIT_TAP_PARSER" parse < "$OUTFILE"
