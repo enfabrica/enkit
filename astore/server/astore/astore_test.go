dcbe463e9d52cd9b6e63d3fb9692a3527be256c2
aaahrens <adam@enfabrica.net>
2021-02-24 16:25:37 -0800

    Working on cleaning up and deferring

--- astore/server/astore/astore_test.go
+++ astore/server/astore/astore_test.go
@@ -1,25 +1,25 @@
 package astore
 
 import (
-	"bufio"
 	"context"
 	"errors"
 	"fmt"
 	"github.com/enfabrica/enkit/astore/client/astore"
 	rpcAstore "github.com/enfabrica/enkit/astore/rpc/astore"
 	"github.com/enfabrica/enkit/lib/client/ccontext"
+	"github.com/enfabrica/enkit/lib/khttp/ktest"
 	"github.com/enfabrica/enkit/lib/logger"
 	"github.com/enfabrica/enkit/lib/progress"
+	"github.com/enfabrica/enkit/lib/srand"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
-	"io"
+	"google.golang.org/grpc/test/bufconn"
 	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
 	"os"
 	"os/exec"
-	"syscall"
 	"testing"
 )
 
@@ -44,11 +44,13 @@ func TestUid(t *testing.T) {
 
 //TODO fix client so that it's signed urls can depend on an interface for actual e2e testing
 func TestServer(t *testing.T) {
-	astoreDescriptor, end, err := RunAStoreServer()
+	astoreDescriptor, killFuncs, err := ktest.RunAStoreServer()
+	if killFuncs != nil {
+		defer killFuncs.KillAll()
+	}
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-	defer end()
 	//running this as test ping feature
 	client := astore.New(astoreDescriptor.Connection)
 	res, _, err := client.List("/test", astore.ListOptions{})
@@ -99,146 +101,4 @@ func TestServer(t *testing.T) {
 
 	fmt.Println("finalizzing +%v", resp.Artifact)
 
-	//ctx := context.Background()
-	//res, err := server.Store(ctx, &storeRequest )
-	//if err != nil {
-	//	t.Error(err.Error())
-	//	return
-	//}
-	//if res.Url == "" || res.Sid == "" {
-	//	t.Error("url or sid not valid")
-	//	return
-	//}
-	//server.Delete()
-
-}
-
-func CheckCredentials() (string, error) {
-	b, err := ioutil.ReadFile("credentials/creds.json")
-	if err != nil {
-		return string(b), err
-	}
-	return string(b), err
-}
-
-//RunMinioServer will spin up a minio serer using the local docker daemon
-//it also returns a func to call that will close and destroy the running image
-//the port and network bind are determined by docker and returned
-func RunAStoreServer() (AStoreDescriptor, func(), error) {
-	y := exec.Command("echo", "$PATH")
-	y.Stdout = os.Stdout
-	err := y.Run()
-	if err != nil {
-		log.Println(err.Error())
-	} else {
-		p, _ := y.Output()
-		fmt.Println("path is ", string(p))
-	}
-	fmt.Println(os.Getenv("PATH"))
-	err = os.Setenv("DATASTORE_EMULATOR_HOST", "localhost:8081")
-	if err != nil {
-		return AStoreDescriptor{}, nil, err
-	}
-	listener, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	allocatedDatastorePort := listener.Addr().(*net.TCPAddr).Port
-	cmd := exec.Command("gcloud",
-		"beta", "emulators", "datastore", "start",
-		"--no-store-on-disk",
-		fmt.Sprintf("--host-port=127.0.0.1:%d", allocatedDatastorePort),
-		"--quiet")
-
-	//cmd.Stdout = os.Stdout
-	//cmd.Stdin = os.Stdin
-	//cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	//doneChannel := make(chan bool)
-	go func() {
-		//err := cmd.Run()
-		//if err != nil {
-		//	log.Fatalln("could not run emulator", err.Error())
-		//}
-		//fmt.Println("hello world")
-		//for {
-		//	b, err := cmd.Output()
-		//	if err != nil {
-		//		log.Fatal(err.Error())
-		//	}
-		//	println("output: " + string(b))
-		//	stringOutput := string(b)
-		//	select {
-		//	case x, ok := <-doneChannel:
-		//		if ok {
-		//			fmt.Printf("Value %d was read.\n", x)
-		//			doneChannel <- true
-		//			continue
-		//		} else {
-		//			fmt.Println("Channel closed!")
-		//			doneChannel <- true
-		//		}
-		//	default:
-		//		if strings.Contains(stringOutput, "Dev App Server is now running.") {
-		//			doneChannel <- true
-		//		}
-		//	}
-		//}
-	}()
-	outputPipe, err := cmd.StdoutPipe()
-	go io.Copy(os.Stdout, outputPipe)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	err = cmd.Start()
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	scanner := bufio.NewScanner(outputPipe)
-	if scanner.Sc  an() {
-		fmt.Println(scanner.Text())
-	}
-	//
-	//err = os.Setenv("STORAGE_EMULATOR_HOST", "localhost:9000")
-	//if err != nil {
-	//	return AStoreDescriptor{}, nil, err
-	//}
-	//
-	//os.Getenv("STORAGE_EMULATOR_HOST")
-	//buffListener := bufconn.Listen(2048 * 2048)
-	//bufDialer := func(context.Context, string) (net.Conn, error) {
-	//	return buffListener.Dial()
-	//}
-	//server, err := New(rand.New(srand.Source),
-	//	WithCredentialsJSON([]byte(``)),
-	//	WithSigningJSON([]byte(``)),
-	//	WithBucket("example-bucket"))
-	//
-	//if err != nil {
-	//	return AStoreDescriptor{}, nil, err
-	//}
-	//grpcServer := grpc.NewServer()
-	//rpcAstore.RegisterAstoreServer(grpcServer, server)
-	////authServer, err := auth2.New(
-	////	rand.New(srand.Source),
-	////	auth2.WithAuthURL("http://empty"))
-	////auth.RegisterAuthServer(grpcServer, authServer)
-	//conn, err := grpc.DialContext(context.Background(),
-	//	"empty", grpc.WithContextDialer(bufDialer),
-	//	grpc.WithInsecure())
-	//
-	//a := func() {
-	//	cmd.Process.Kill()
-	//	if err := grpcServer.Serve(buffListener); err != nil {
-	//		log.Fatalf("Server exited with error: %v", err)
-	//	}
-	//}
-	return AStoreDescriptor{
-	}, nil, nil
-}
-
-type AStoreDescriptor struct {
-	Connection *grpc.ClientConn
-	Server     *Server
 }
