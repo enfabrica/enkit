23860fb011ea881fbbc3478295b334fb6fef3482
Adam Ahrens <adam@enfabrica.net>
2021-03-09 13:49:18 -0800

    cleaning up testing and testing rsa byte outputs

--- lib/kcerts/certs.go
+++ lib/kcerts/certs.go
@@ -1,2 +1,202 @@
 package kcerts
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"math/big"
+	"net"
+	"time"
+)
+
+//GenerateNewCARoot returns the new
+func GenerateNewCARoot(opts *CertOptions) (*x509.Certificate, []byte, *rsa.PrivateKey, error) {
+	if !opts.isValid {
+		return nil, nil, nil, errors.New("must call Validate() on certificate options before creating a CA")
+	}
+	rootTemplate := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Country:      opts.Country,
+			Organization: opts.Organization,
+			CommonName:   "Root CA",
+		},
+		NotBefore:             opts.Before,
+		NotAfter:              opts.After,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLen:            2,
+		IPAddresses:           opts.IPAddresses,
+	}
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	certBytes, err := x509.CreateCertificate(rand.Reader, &rootTemplate, &rootTemplate, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
+	certPEM := pem.EncodeToMemory(&b)
+
+	return &rootTemplate, certPEM, privateKey, nil
+}
+
+//GenerateIntermediateCertificate
+func GenerateIntermediateCertificate(opts *CertOptions, RootCa *x509.Certificate, RootCaPrivateKey *rsa.PrivateKey) (*x509.Certificate, []byte, *rsa.PrivateKey, error) {
+	intermediatePrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if !opts.isValid {
+		return nil, nil, nil, errors.New("must call Validate() on certificate options before creating a DCA")
+	}
+	intermediate := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Country:      opts.Country,
+			Organization: opts.Organization,
+			CommonName:   "DCA",
+		},
+		NotBefore:             opts.Before,
+		NotAfter:              opts.After,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+		MaxPathLenZero:        false,
+		MaxPathLen:            1,
+		IPAddresses:           opts.IPAddresses,
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, &intermediate, RootCa, &intermediatePrivateKey.PublicKey, RootCaPrivateKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
+	certPEM := pem.EncodeToMemory(&b)
+
+	return cert, certPEM, intermediatePrivateKey, nil
+}
+
+// GenerateServerKey will generate the final tls cert, generally requires GenerateIntermediateCertificate and
+// GenerateNewCARoot to be called beforehand. Reusing Opts is recommended.
+func GenerateServerKey(opts *CertOptions, intermediateCert *x509.Certificate, intermediatePrivateKey *rsa.PrivateKey) (*x509.Certificate, []byte, *rsa.PrivateKey, error) {
+	serverPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if !opts.isValid {
+		return nil, nil, nil, errors.New("must call Validate() on certificate options before creating a Server Crt")
+	}
+
+	serverTemplate := &x509.Certificate{
+		SerialNumber:   big.NewInt(1),
+		NotBefore:      opts.Before,
+		NotAfter:       opts.After,
+		KeyUsage:       x509.KeyUsageCRLSign,
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IsCA:           false,
+		MaxPathLenZero: true,
+		IPAddresses:    opts.IPAddresses,
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, serverTemplate, intermediateCert, &serverPrivateKey.PublicKey, intermediatePrivateKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	cert, err := x509.ParseCertificate(certBytes)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certBytes}
+	certPEM := pem.EncodeToMemory(&b)
+
+	return cert, certPEM, serverPrivateKey, nil
+
+}
+
+// GenerateSSHKeyPair will generate a re-encoded private rsa key and public key from an existing *rsa.PrivateKey.
+func GenerateSSHKeyPair(privateKey *rsa.PrivateKey) ([]byte, []byte) {
+	privateBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	publicBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(&privateKey.PublicKey),
+	}
+	return pem.EncodeToMemory(publicBlock), pem.EncodeToMemory(privateBlock)
+}
+
+type CertOptions struct {
+	Country      []string
+	Organization []string
+	Before       time.Time
+	After        time.Time
+	IPAddresses  []net.IP
+	isValid      bool
+}
+
+func (o *CertOptions) WithCountries(cs []string) *CertOptions {
+	o.Country = cs
+	return o
+}
+
+func (o *CertOptions) WithOrganizations(orgs []string) *CertOptions {
+	o.Organization = orgs
+	return o
+}
+
+func (o *CertOptions) WithIpAddresses(ips []net.IP) *CertOptions {
+	o.IPAddresses = ips
+	return o
+}
+
+func (o *CertOptions) ValidUntil(time2 time.Time) *CertOptions {
+	o.After = time2
+	return o
+}
+func (o *CertOptions) NotValidBefore(time2 time.Time) *CertOptions {
+	o.Before = time2
+	return o
+}
+
+func (o *CertOptions) Validate() error {
+	currTime := time.Now()
+	if currTime.Before(o.Before) {
+		return errors.New("time must be before time.Now")
+	}
+	if currTime.After(o.After) {
+		return errors.New("cannot issue invalid CA's time invalid")
+	}
+	if o.After.Sub(currTime).Hours() < 24*365 { // hours in a year
+		return errors.New("duration of the CA is too low")
+	}
+	if len(o.Organization) == 0 {
+		return errors.New("must set organization")
+	}
+	if len(o.Country) == 0 {
+		return errors.New("must set countries of origin for en_Lang and i8n support")
+	}
+	o.isValid = true
+	return nil
+}
+
+func NewOptions() *CertOptions {
+	return &CertOptions{}
+}
--- lib/kcerts/certs_test.go
+++ lib/kcerts/certs_test.go
@@ -1,6 +1,126 @@
 package kcerts_test
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"github.com/enfabrica/enkit/lib/kcerts"
+	"golang.org/x/crypto/ssh"
+	"log"
+	"net"
+	"testing"
+	"time"
+)
 
+func TestCertSuite(t *testing.T) {
+	opts := kcerts.NewOptions().
+		NotValidBefore(time.Now().Add(-5 * time.Minute)).
+		ValidUntil(time.Now().AddDate(3, 0, 0)).
+		WithIpAddresses([]net.IP{net.ParseIP("127.0.0.1"), net.ParseIP("0.0.0.0")}).
+		WithOrganizations([]string{"Test Corp"}).
+		WithCountries([]string{"US"})
 
+	err := opts.Validate()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	rootCert, rootPem, rootPrivateKey, err := kcerts.GenerateNewCARoot(opts)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	intermediateCert, intermediatePem, intermediatePrivateKey, err := kcerts.GenerateIntermediateCertificate(opts, rootCert, rootPrivateKey)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 
+	serverCert, _, _, err := kcerts.GenerateServerKey(opts, intermediateCert, intermediatePrivateKey)
+	t.Run("verify intermediate", func(t *testing.T) {
+		if err := verifyIntermediateChain(rootPem, intermediateCert); err != nil {
+			t.Error("error verifying intermediate chain", err)
+		}
 
+	})
+	t.Run("verify chain", func(t *testing.T) {
+		if err := verifyFullChain(rootPem, intermediatePem, serverCert); err != nil {
+			t.Error("error verifying full chain", err)
+		}
+	})
+	t.Run("verify rsa client output with intermediate chain", func(t *testing.T) {
+		if err := verifyRSAEncryption(intermediatePrivateKey); err != nil {
+			t.Error(err.Error())
+		}
+	})
+	t.Run("verify rsa client output with root chain", func(t *testing.T) {
+		if err := verifyRSAEncryption(rootPrivateKey); err != nil {
+			t.Error(err.Error())
+		}
+	})
+
+	t.Run("test rsa output generations", func(t *testing.T) {
+		publicBytes, privateBytes := kcerts.GenerateSSHKeyPair(rootPrivateKey)
+		publicBlock, _ := pem.Decode(publicBytes)
+		if publicBlock.Type != "PUBLIC KEY" {
+			t.Error("public key is re encoded block, check encoding steps")
+		}
+		privateBlock, _ := pem.Decode(privateBytes)
+		if privateBlock.Type != "RSA PRIVATE KEY" {
+			t.Error("private key is re encoded block or password encrypted")
+		}
+		privateKey, err := x509.ParsePKCS1PrivateKey(privateBlock.Bytes)
+		if err != nil {
+			t.Error("error parsing private key bytes ", err.Error())
+		}
+		if err := verifyRSAEncryption(privateKey); err != nil {
+			t.Error(err.Error())
+		}
+	})
+}
+
+func verifyIntermediateChain(root []byte, inter *x509.Certificate) error {
+	roots := x509.NewCertPool()
+	roots.AppendCertsFromPEM(root)
+	opts := x509.VerifyOptions{
+		Roots: roots,
+	}
+
+	if _, err := inter.Verify(opts); err != nil {
+		return err
+	}
+	return nil
+}
+
+func verifyFullChain(root, inter []byte, child *x509.Certificate) error {
+	roots := x509.NewCertPool()
+	inters := x509.NewCertPool()
+	roots.AppendCertsFromPEM(root)
+	inters.AppendCertsFromPEM(inter)
+	opts := x509.VerifyOptions{
+		Roots:         roots,
+		Intermediates: inters,
+	}
+	if _, err := child.Verify(opts); err != nil {
+		return err
+	}
+	return nil
+}
+
+func verifyRSAEncryption(key *rsa.PrivateKey) error {
+	data := []byte("Hello, world!")
+	signer, _ := ssh.NewSignerFromKey(key)
+	sig, _ := signer.Sign(rand.Reader, data)
+
+	// extract the ssh.PublicKey from *rsa.PublicKey to verify the signature
+	pub, _ := ssh.NewPublicKey(&key.PublicKey)
+	if err := pub.Verify(data, sig); err != nil {
+		log.Fatalf("publicKey.Verify failed: %v", err)
+	}
+	// modify the data and make sure we get a failure
+	data[0]++
+	if err := pub.Verify(data, sig); err == nil {
+		return errors.New("modifying the data should have resulted in a verification error")
+	}
+
+	return nil
+}
