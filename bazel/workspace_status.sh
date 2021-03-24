0da6f494f1fbcb5e3d98743128c0df4536c1df88
aaahrens <adam@enfabrica.net>
2021-03-24 02:10:43 -0700

    address ci failing, generic user and old git

--- bazel/workspace_status.sh
+++ bazel/workspace_status.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
-CHANGED_FILES=$(git --no-pager diff --name-only origin/master $(git --no-pager branch --show-current))
+CHANGED_FILES=$(git --no-pager diff --name-only origin/master $(git rev-parse --abbrev-ref HEAD))
 GIT_SHA=$(git rev-parse HEAD)
-GIT_USER=$(git config --get user.name)
-GIT_EMAIL=$(git config --get user.email)
+GIT_USER=$(git config --get user.name 2>&1 || true)
+GIT_EMAIL=$(git config --get user.email 2>&1 || true)
+USER=$USER
 
 echo GIT_CHANGED_FILES $CHANGED_FILES
 echo GIT_SHA $GIT_SHA
 echo GIT_USER $GIT_USER
 echo GIT_EMAIL $GIT_EMAIL
+echo USER $USER
