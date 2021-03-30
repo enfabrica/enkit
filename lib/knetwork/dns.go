772b3eef30aae9a36ef70a34725bed6d97a09d25
aaahrens <adam@enfabrica.net>
2021-03-30 16:20:14 -0700

    add dns server to knetwork

--- lib/knetwork/dns.go
+++ lib/knetwork/dns.go
@@ -1,75 +1,140 @@
 package knetwork
 
 import (
-	"github.com/miekg/dns"
 	"errors"
+	"fmt"
+	"github.com/enfabrica/enkit/lib/logger/klog"
+	"github.com/miekg/dns"
+	"net"
 	"strconv"
 	"sync"
 )
 
 var (
 	DNSEntryNotExistError = errors.New("the following entry did not exist")
+	DNSEntryExistError    = errors.New("the following entry did exist")
 )
 
-func NewDNS() *DnsServer {
-	return &DnsServer{
-		routeMap:  make(map[string][]string),
-		dnsServer: dns.NewServeMux(),
+func NewDNS(mods ...DNSModifier) (*DnsServer, error) {
+	defaultLogger, err := klog.New("default", klog.FromFlags(*klog.DefaultFlags()))
+	if err != nil {
+		return nil, err
+	}
+	s := &DnsServer{
+		routeMap: make(map[string][]string),
+		Logger:   defaultLogger,
+	}
+	for _, mod := range mods {
+		if err := mod(s); err != nil {
+			return nil, err
+		}
+	}
+	return s, nil
+}
+
+type DNSModifier func(s *DnsServer) error
+
+func WithLogger(l *klog.Logger) DNSModifier {
+	return func(s *DnsServer) error {
+		s.Logger = l
+		return nil
 	}
 }
 
-func WithLogger() {
+func WithPort(p int) DNSModifier {
+	return func(s *DnsServer) error {
+		s.Port = p
+		return nil
+	}
+}
 
+func WithDomains(domains []string) DNSModifier {
+	return func(s *DnsServer) error {
+		s.domains = domains
+		return nil
+	}
+}
+
+func WithListener(l net.Listener) DNSModifier {
+	return func(s *DnsServer) error {
+		s.Listener = l
+		return nil
+	}
 }
 
 type DnsServer struct {
-	dnsServer *dns.ServeMux
+	dnsServer *dns.Server
 	sync.RWMutex
 	routeMap map[string][]string
 	Port     int
+	Logger   *klog.Logger
+	domains  []string
+	Listener net.Listener
 }
 
 func (s *DnsServer) Start() error {
-	s.dnsServer.HandleFunc("service.", s.HandleIncoming)
-	return dns.ListenAndServe(":"+strconv.Itoa(s.Port), "udp", s.dnsServer)
+	mux := dns.NewServeMux()
+	for _, domain := range s.domains {
+		mux.HandleFunc(dns.Fqdn(domain), s.HandleIncoming)
+	}
+	s.dnsServer = &dns.Server{Handler: mux}
+	if s.Listener != nil {
+		s.dnsServer.Listener = s.Listener
+		s.dnsServer.Net = "udp"
+		s.dnsServer.Addr = s.Listener.Addr().String()
+	}
+	if s.Port != 0 {
+		s.dnsServer.Addr = net.JoinHostPort("127.0.0.1", strconv.Itoa(s.Port))
+	}
+	return s.dnsServer.ListenAndServe()
 }
 
 func (s *DnsServer) Stop() error {
-
+	return s.dnsServer.Shutdown()
 }
 
-func (s *DnsServer) AddEntry() error {
-
+func (s *DnsServer) AddEntry(name string, ips []string) error {
+	if s.routeMap[dns.Fqdn(name)] != nil {
+		return DNSEntryNotExistError
+	}
+	s.Lock()
+	s.routeMap[dns.Fqdn(name)] = ips
+	s.Unlock()
+	return nil
 }
 
-func (s *DnsServer) ReplaceEntry() error {
-
+func (s *DnsServer) ReplaceEntry(name string, ips []string) error {
+	s.Lock()
+	s.routeMap[name] = ips
+	s.Unlock()
+	return nil
 }
 
-func AppendToEntry() error {
-
-}
-
-func (s *DnsServer) RemoveIPFromEntry() error {
-
-}
-
-func RemoveEntry() error {
-
-}
-
-func (s DnsServer) HandleIncoming(writer dns.ResponseWriter, incoming *dns.Msg) {
+func (s *DnsServer) HandleIncoming(writer dns.ResponseWriter, incoming *dns.Msg) {
 	m := &dns.Msg{}
 	m.SetReply(incoming)
 	m.Compress = false
-
 	switch incoming.Opcode {
 	case dns.OpcodeQuery:
-		parseQuery(m)
+		s.ParseDNS(m)
 	}
 	err := writer.WriteMsg(m)
+	if err != nil {
+		s.Logger.Errorw(err.Error())
+	}
 }
 
-func ParseDNS() {
-
+func (s *DnsServer) ParseDNS(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			ips := s.routeMap[q.Name]
+			for _, ip := range ips {
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		}
+	}
 }
--- lib/knetwork/dns_test.go
+++ lib/knetwork/dns_test.go
@@ -1,13 +1,45 @@
 package knetwork_test
 
 import (
+	"context"
+	"fmt"
 	"github.com/enfabrica/enkit/lib/knetwork"
+	"github.com/stretchr/testify/assert"
+	"net"
 	"testing"
+	"time"
 )
 
-func TestDNS(t *testing.T){
-	dnsServer := knetwork.NewDNS()
-	go func() {
+func TestDNS(t *testing.T) {
+	// Setup
+	l, err := knetwork.AllocatePort()
+	assert.Nil(t, err)
 
+	dnsServer, err := knetwork.NewDNS(
+		knetwork.WithDomains([]string{"enkit"}),
+		knetwork.WithListener(l),
+	)
+	fmt.Println(l.Addr().String())
+	customResolver := &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{
+				Timeout: time.Millisecond * time.Duration(10000),
+			}
+			return d.DialContext(ctx, network, l.Addr().String())
+		},
+	}
+	go func() {
+		assert.Nil(t, dnsServer.Start())
 	}()
+	defer dnsServer.Stop()
+
+	// Actual Test
+	err = dnsServer.AddEntry("hello.enkit", []string{"10.9.9.9", "10.90.80.70"})
+	assert.Nil(t, err)
+	time.Sleep(150 * time.Millisecond)
+
+	ips, err := customResolver.LookupHost(context.TODO(), "hello.enkit")
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(ips))
 }
