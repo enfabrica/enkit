89dd0ea32bc10256456aa01dafa011afdcacb2f4
Brian Huynh <bbhuynh@enfabrica.net>
2021-04-15 00:46:59 +0000

    Addressed 1st round of suggestions

---
+++ manager/client/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"os/exec"
+	"time"
+	"log"
+	"context"
+	"google.golang.org/grpc"
+	rpc_license "github.com/enfabrica/enkit/manager/rpc"
+)
+
+func run(status chan bool, cmd string, args ...string) error {
+	job := exec.Command(cmd, args...)
+	err := job.Run()
+	if err != nil {
+		log.Fatalf("Build failed to complete: %s \n", err)
+		status <- true
+		close(status)
+		return err
+	}
+	status <- false
+	close(status)
+	return nil
+}
+
+func keepalive(status chan bool, client rpc_license.LicenseClient, hash string, vendor string, feature string) {
+	stream, err := client.KeepAlive(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get stream object: %s \n", err)
+	}
+	for {
+		stream.Send(&rpc_license.KeepAliveMessage{Hash: hash, Vendor: vendor, Feature: feature})
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if recv.Hash != hash {
+			log.Fatalf("Hash from server does not match client: %s \n", recv.Hash)
+		}
+		if err != nil {
+			log.Fatalf("Error receiving response: %s \n", err)
+		}
+		time.Sleep(5 * time.Second)
+	}
+	// failure if keepalive is disconnected before go-routine run() finishes
+	status <- true
+	close(status)
+}
+
+func polling(client rpc_license.LicenseClient, username string, quantity int32, vendor string, feature string) (string, error) {
+	timeout := 1800 * time.Second
+	waiting := 0 * time.Second
+	interval := 5 * time.Second
+	stream, err := client.Polling(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get stream object: %s \n", err)
+	}
+	hash := ""
+	for waiting < timeout {
+		request := rpc_license.PollingRequest{Vendor: vendor, Feature: feature, Quantity: quantity, User: username, Hash: hash}
+		err := stream.Send(&request)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error sending message: %s \n", err)
+		}
+		recv, err := stream.Recv()
+		hash = recv.Hash
+		if recv.Acquired || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error receiving response: %s \n", err)
+		}
+		log.Printf("Client %s waiting for %d %s feature %s license \n", hash, quantity, vendor, feature)
+		time.Sleep(interval)
+		waiting += interval
+	}
+	if waiting >= timeout {
+		log.Fatalf("Failed to acquire %d %s feature %s license after %d seconds \n", quantity, vendor, feature, waiting)
+	}
+	return hash, nil
+}
+
+func main() {
+	var quantity int32 = 1
+	host, port := os.Args[1], os.Args[2]
+	vendor, feature := os.Args[3], os.Args[4]
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", host, port), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Connection failed: %s \n", err)
+	}
+	client := rpc_license.NewLicenseClient(conn)
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf("Failed to get username: %s \n", err)
+	}
+	hash, err := polling(client, user.Username, quantity, vendor, feature)
+	if err == nil {
+		log.Printf("Successfully acquired %d %s feature %s license from server \n", quantity, vendor, feature)
+		status := make(chan bool)
+		go run(status, os.Args[5], os.Args[6:]...)
+		go keepalive(status, client, hash, vendor, feature)
+		// blocking call waiting for channel status
+		isFailure := <-status
+		if isFailure {
+			os.Exit(1)
+		} else {
+			os.Exit(0)
+		}
+	} else {
+		log.Fatalf("Invalid response from server: %s \n", err)
+	}
+	defer conn.Close()
+}
--- manager/common/license.go
+++ manager/common/license.go
@@ -1,62 +1,175 @@
 package license
 import (
+	"io"
 	"log"
 	"sync"
-	"context"
+	"time"
+	"strings"
+	"math/rand"
+	"google.golang.org/grpc/metadata"
 	rpc_license "github.com/enfabrica/enkit/manager/rpc"
+	astoreServer "github.com/enfabrica/enkit/astore/server/astore"
 )
 
 type LicenseCounter struct {
-    mutex sync.Mutex
-    counter int
-    total int
+    counterMutex sync.Mutex
+    counter map[string]int // current number of licenses used per vendor
+	clientMutex sync.Mutex
+	clients map[string]*Client // client connection metadata
+    total map[string]int // total licenses available per vendor
+	queue []string
 }
 
-func (c *LicenseCounter) increment(num int) int {
-    c.mutex.Lock()
-    if c.counter < c.total {
-        c.counter += num
-    }
-    c.mutex.Unlock()
-    return c.counter
-}
-
-func (c *LicenseCounter) decrement(num int) int {
-	c.mutex.Lock()
-	if c.counter - num >= 0 {
-		c.counter -= num
+func (c *LicenseCounter) increment(vendor string, num int, hash string) int {
+    c.counterMutex.Lock()
+    if c.counter[vendor] < c.total[vendor] {
+		log.Printf("License acquired by %s \n", hash)
+        c.counter[vendor] += num
+    } else {
+		log.Fatalf("Cannot acquire more licenses than available \n")
 	}
-	c.mutex.Unlock()
-	return c.counter
+    defer c.counterMutex.Unlock()
+    return c.counter[vendor]
 }
 
-var totalLicenses = 3
-var licenseCounter = LicenseCounter{counter: 0, total: totalLicenses}
+func (c *LicenseCounter) decrement(vendor string, num int, hash string) int {
+	c.counterMutex.Lock()
+	if c.counter[vendor] - num >= 0 {
+		log.Printf("License released by %s \n", hash)
+		c.counter[vendor] -= num
+	} else {
+		log.Fatalf("Cannot release a negative number of licenses \n")
+	}
+	defer c.counterMutex.Unlock()
+	return c.counter[vendor]
+}
+
+func (c *LicenseCounter) enqueue(key string, val *Client) int {
+	c.clientMutex.Lock()
+	c.queue = append(c.queue, key)
+	c.clients[key] = val
+	defer c.clientMutex.Unlock()
+	return len(c.queue)
+}
+
+func (c *LicenseCounter) dequeue(key string) {
+	c.clientMutex.Lock()
+	c.queue = c.queue[1:]
+	delete(c.clients, key)
+	defer c.clientMutex.Unlock()
+}
+
+func (c *LicenseCounter) del(key string) int {
+	index := -1
+	c.clientMutex.Lock()
+	for i := 0; i < len(c.queue); i++ {
+		if c.queue[i] == key {
+			c.queue = append(c.queue[:i], c.queue[i+1:]...)
+			delete(c.clients, key)
+			log.Printf("Client %s removed from queue \n", key)
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		log.Fatalf("%s is not in queue \n", key)
+	}
+	defer c.clientMutex.Unlock()
+	return index
+}
+
+type Client struct {
+	IP string
+	User string
+	Start time.Time
+}
+
+var clients = make(map[string]*Client)
+var totalCadenceLic = 3
+var totalXilinxLic = 3
+var licenseCounter = LicenseCounter{counter: map[string]int{"xilinx": 0, "cadence": 0},
+									total: map[string]int{"xilinx": totalXilinxLic, "cadence": totalCadenceLic},
+									clients: make(map[string]*Client),
+									queue: make([]string, 0)}
 type Server struct {
 }
 
-func (s *Server) Acquire(ctx context.Context, request *rpc_license.AcquireRequest) (*rpc_license.AcquireResponse, error) {
-	log.Printf("Received request for %d %s feature %s \n", request.Quantity, request.Vendor, request.Feature)
-	if licenseCounter.counter < licenseCounter.total {
-		log.Printf("Licenses in use: %d/%d \n", licenseCounter.increment(int(request.Quantity)), licenseCounter.total)
-		return &rpc_license.AcquireResponse{Available: true, Waiting: false, Missing: false}, nil
-	} else {
-		return &rpc_license.AcquireResponse{Available: false, Waiting: true, Missing: false}, nil
+func (s *Server) KeepAlive(stream rpc_license.License_KeepAliveServer) error {
+	var status error
+	var hash string
+	var vendor string
+	var feature string
+	for {
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			status = nil
+			break
+		}
+		if err != nil {
+			status = err
+			break
+		}
+		hash, vendor, feature = recv.Hash, recv.Vendor, recv.Feature
+		err = stream.Send(&rpc_license.KeepAliveMessage{Hash: hash, Vendor: vendor, Feature: feature})
+		if err != nil {
+			status = err
+			break
+		}
 	}
+	inUse := licenseCounter.decrement(vendor, 1, hash)
+	log.Printf("%s %s licenses in use: %d/%d \n", vendor, feature, inUse, licenseCounter.total[vendor])
+	return status
 }
 
-func (s *Server) Release(ctx context.Context, request *rpc_license.ReleaseRequest) (*rpc_license.ReleaseResponse, error) {
-	log.Printf("Received request to release %d %s feature %s \n", request.Quantity, request.Vendor, request.Feature)
-	if licenseCounter.counter - int(request.Quantity) >= 0 {
-		log.Printf("Licenses in use: %d/%d \n", licenseCounter.decrement(int(request.Quantity)), licenseCounter.total)
-		return &rpc_license.ReleaseResponse{Success: true}, nil
-	} else {
-		return &rpc_license.ReleaseResponse{Success: false}, nil
-	}
-}
+func (s *Server) Polling(stream rpc_license.License_PollingServer) error {
+	var status error
+	var hash string
+	for {
+		recv, err := stream.Recv()
+		if err == io.EOF {
+			status = nil
+			break
+		}
+		if err != nil {
+			status = err
+			break
+		}
+		md, ok := metadata.FromIncomingContext(stream.Context())
+		if !ok {
+			log.Fatalf("grpc metadata not ok \n")
+		}
+		ip := strings.Join(md[":authority"], " ")
+		username := recv.User
+		start := time.Now().UTC()
+		if recv.Hash == "" {
+			rng := rand.New(rand.NewSource(rand.Int63()))
+			hash, err = astoreServer.GenerateUid(rng)
+			if err != nil {
+				log.Fatalf("Failed to generate hash for %s@%s", recv.User, ip)
+			}
+			length := licenseCounter.enqueue(hash, &Client{IP: ip, User: username, Start: start})
+			log.Printf("Client %s from %s@%s received at %s waiting at position %d \n", hash, username, ip, start, length)
+		} else {
+			hash = recv.Hash
+		}
+		log.Printf("Received request for %d %s feature %s from client %s \n", recv.Quantity, recv.Vendor, recv.Feature, hash)
+		if licenseCounter.counter[recv.Vendor] < licenseCounter.total[recv.Vendor] && licenseCounter.queue[0] == hash {
+			log.Printf("%s %s licenses in use: %d/%d \n", recv.Vendor, recv.Feature,
+													      licenseCounter.increment(recv.Vendor, int(recv.Quantity), hash),
+													      licenseCounter.total[recv.Vendor])
+			licenseCounter.dequeue(hash)
+			log.Printf("Client %s removed from the queue \n", hash)
+			return stream.Send(&rpc_license.PollingResponse{Acquired: true, Hash: hash})
+		} else {
+			log.Printf("Client %s waiting for %d %s feature %s to be available \n", hash, recv.Quantity, recv.Vendor, recv.Feature)
+			err := stream.Send(&rpc_license.PollingResponse{Acquired: false, Hash: hash})
+			if err != nil {
+				log.Fatalf("Failed to send message back to client: %s \n", err)
+			}
+		}
 
-func (s *Server) KeepAlive(ctx context.Context, request *rpc_license.KeepAliveRequest) (*rpc_license.KeepAliveResponse, error) {
-	if request.Heartbeat {
-		log.Printf("Received heartbeat \n")
 	}
+	// remove client hash from queue when connection is broken
+	licenseCounter.del(hash)
+	return status
 }
