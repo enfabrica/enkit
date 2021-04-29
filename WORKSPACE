f78c01ce3cedb6247d2d90a0e73d7a698148faee
Brian Huynh <bbhuynh@enfabrica.net>
2021-04-29 16:08:58 -0700

    Work in Progress: License Manager (#55)
    First draft of generic license manager for EDA tools especially for Xilinx. Not necessary for Cadence tools because -licqueue argument automatically queues Cadence jobs and polls for a license.

--- WORKSPACE
+++ WORKSPACE
@@ -38,6 +38,34 @@ http_archive(
     url = "https://github.com/bazelbuild/buildtools/archive/master.zip",
 )
 
+http_archive(
+    name = "io_bazel_rules_docker",
+    sha256 = "4521794f0fba2e20f3bf15846ab5e01d5332e587e9ce81629c7f96c793bb7036",
+    strip_prefix = "rules_docker-0.14.4",
+    urls = ["https://github.com/bazelbuild/rules_docker/releases/download/v0.14.4/rules_docker-v0.14.4.tar.gz"],
+)
+load(
+    "@io_bazel_rules_docker//repositories:repositories.bzl",
+    container_repositories = "repositories",
+)
+
+container_repositories()
+
+load("@io_bazel_rules_docker//repositories:deps.bzl", container_deps = "deps")
+
+container_deps()
+
+load("@io_bazel_rules_docker//repositories:pip_repositories.bzl", "pip_deps")
+
+pip_deps()
+load("@io_bazel_rules_docker//container:pull.bzl", "container_pull")
+container_pull(
+    name = "golang_base",
+    digest = "sha256:75f63d4edd703030d4312dc7528a349ca34d48bec7bd754652b2d47e5a0b7873",
+    registry = "gcr.io",
+    repository = "distroless/base",
+)
+
 load("@build_bazel_rules_nodejs//:index.bzl", "yarn_install")
 
 yarn_install(
---
+++ manager/client/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	rpc_license "github.com/enfabrica/enkit/manager/rpc"
+	"google.golang.org/grpc"
+	grpcCodes "google.golang.org/grpc/codes"
+	grpcStatus "google.golang.org/grpc/status"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+	"time"
+)
+
+func run(timeout time.Duration, cmd string, args ...string) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	job := exec.CommandContext(ctx, cmd, args...)
+	err := job.Run()
+	if ctx.Err() == context.DeadlineExceeded {
+		log.Fatalf("Job failed to complete after %d seconds: %s %s \n", timeout, cmd, strings.Join(args, " "))
+	}
+	if err != nil {
+		log.Fatalf("Job \"%s %s\" failed with error %s", cmd, strings.Join(args, " "), err)
+	}
+	log.Printf("Job completed successfully: %s %s \n", cmd, strings.Join(args, " "))
+}
+
+func polling(client rpc_license.LicenseClient, username string, quantity int32, vendor string, feature string,
+	cmd string, args ...string) {
+	timeout := 1800 * time.Second
+	waiting := 0 * time.Second
+	interval := 5 * time.Second
+	stream, err := client.Polling(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to get stream object: %s \n", err)
+	}
+	hash := ""
+	acquired := false
+	for waiting < timeout {
+		request := rpc_license.PollingRequest{Vendor: vendor, Feature: feature, Quantity: quantity, User: username, Hash: hash}
+		err := stream.Send(&request)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error sending  message: %s \n", err)
+		}
+		recv, err := stream.Recv()
+		if err != nil {
+			errStatus, _ := grpcStatus.FromError(err)
+			if errStatus.Code() == grpcCodes.InvalidArgument {
+				log.Fatalf(errStatus.Message())
+			} else if err == io.EOF {
+				break
+			} else {
+				log.Fatalf("Error receiving message: %s \n", err)
+			}
+		} else {
+			hash = recv.Hash
+			if recv.Acquired {
+				acquired = recv.Acquired
+				break
+			}
+		}
+		// no need to continuously log after the initial request
+		if waiting == 0 {
+			log.Printf("Client %s waiting for %d %s feature %s license \n", hash, quantity, vendor, feature)
+		}
+		time.Sleep(interval)
+		waiting += interval
+	}
+	if waiting >= timeout {
+		log.Fatalf("Failed to acquire %d %s feature %s license after %d seconds \n", quantity, vendor, feature, waiting)
+	}
+	if acquired {
+		log.Printf("Successfully acquired %d %s feature %s license from server \n", quantity, vendor, feature)
+		// 12 hours in seconds
+		run(60*60*12*time.Second, cmd, args...)
+	}
+}
+
+func main() {
+	var quantity int32 = 1
+	host, port := os.Args[1], os.Args[2]
+	vendor, feature := os.Args[3], os.Args[4]
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", host, port), grpc.WithInsecure())
+	defer conn.Close()
+	if err != nil {
+		log.Fatalf("Connection failed: %s \n", err)
+	}
+	client := rpc_license.NewLicenseClient(conn)
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf("Failed to get username: %s \n", err)
+	}
+	polling(client, user.Username, quantity, vendor, feature, os.Args[5], os.Args[6:]...)
+}
