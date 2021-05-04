dc09afae8611011b640df6620645fffb4ca2db17
aaahrens <adam@enfabrica.net>
2021-05-03 22:39:49 -0700

    typos

--- lib/oauth/oauth.go
+++ lib/oauth/oauth.go
@@ -511,6 +511,7 @@ func (a *Authenticator) ExtractAuth(w http.ResponseWriter, r *http.Request) (Aut
 	if err := a.authEncoder.Decode([]byte(cookie.Value), &secretExpected); err != nil {
 		return AuthData{}, fmt.Errorf("Cookie decoding failed - %w", err)
 	}
+
 	query := r.URL.Query()
 	state := query.Get("state")
 	var received LoginState
