3ca78dea6fedd41bf64ff5ca6a1afad911291a9b
adam <adam@enfabrica.net>
2021-09-21 14:26:26 -0700

    add readme

--- lib/oauth/types.go
+++ lib/oauth/types.go
@@ -4,7 +4,10 @@ import (
 	"github.com/enfabrica/enkit/lib/khttp/kcookie"
 	"net/http"
 )
-
+// An IAuthenticator is any object capable of performing authentication for a web server.
+// PerformLogin initiates the login process.
+// PerformAuth is invoked at the end, to verify that the login was successful.
+// Complete is used to verify that the returned AuthData indicates that the process is now complete.
 type IAuthenticator interface {
 	PerformLogin(w http.ResponseWriter, r *http.Request, lm ...LoginModifier) error
 	PerformAuth(w http.ResponseWriter, r *http.Request, mods ...kcookie.Modifier) (AuthData, error)
