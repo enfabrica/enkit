e111e23f21dd6b5bbf12b77b9a9304ae81c28d2c
Scott Minor <minor@minorhacks.com>
2021-10-05 13:15:59 -0700

    WIP for server implementation

--- license_manager/service/service.go
+++ license_manager/service/service.go
@@ -12,6 +12,15 @@ import (
 type Service struct{}
 
 func (s *Service) Allocate(ctx context.Context, req *lmpb.AllocateRequest) (*lmpb.AllocateResponse, error) {
+	// If invocation is already allocated, return allocation
+	// Otherwise, get spot in queue
+	//   If request has an ID, find that spot in the queue or error
+	//   Otherwise, request goes at the back of the queue for that license
+	// Attempt to allocate available licenses to everyone in queue
+	// If not at the front of the queue, return queue spot
+	// (At the front of the queue)
+	// If license not available, return queue spot
+	// Allocate license and return allocation
 	return nil, status.Errorf(codes.Unimplemented, "Allocate() is not yet implemented")
 }
 
