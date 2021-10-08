fb77aa2167a7312621d1732a2975ba430ebf3401
Scott Minor <scott@enfabrica.net>
2021-10-08 09:55:34 -0700

    git: Add Go lib with temp worktree generation (#235)
    This change adds a git library, with initial support for generating
    worktrees from a provided repo path.
    
    The bazel changed target detector will need the ability to run at two
    different points in the source history locally. One way to do this is to
    issue checkout commands in the user's repository; however, it is
    possible that a crash/error will leave a user's repository in an
    unexpected/unknown state.
    
    Instead, we can shell out to `git worktree` to create essentially a
    shallow clone at a particular committish in a different directory (in
    this case, a temporary directory) and perform bazel queries on these
    temporary worktrees. Here the only risk is that we fail to delete the
    temp worktrees upon crash/error, which are trivially cleaned up later
    manually or via reboot.
    
    go-git does have the ability to traverse a worktree, but it does not
    appear to be trivial to spill a particular commit to disk so that we can
    invoke `bazel` on it.
    
    This change adds a small library that shells out to `git worktree` to
    add/remove the worktree instead.
    
    Tested: Unit tests in this commit
    
    Jira: INFRA-52

---
+++ lib/git/worktree_test.go
@@ -0,0 +1,30 @@
+package git
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/prashantv/gostub"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTempWorktree(t *testing.T) {
+	gotCmds := []*exec.Cmd{}
+	stubs := gostub.Stub(&runCommand, func(cmd *exec.Cmd) ([]byte, error) {
+		gotCmds = append(gotCmds, cmd)
+		return nil, nil
+	})
+	defer stubs.Reset()
+
+	got, gotErr := NewTempWorktree("/foo/bar", "some_branch_name")
+	closeErr := got.Close()
+
+	assert.NoError(t, gotErr)
+	assert.NoError(t, closeErr)
+	assert.Equal(t, 2, len(gotCmds))
+	assert.Equal(t, []string{"git", "worktree", "add"}, gotCmds[0].Args[0:3])
+	assert.Equal(t, []string{"some_branch_name"}, gotCmds[0].Args[4:])
+	assert.Equal(t, "/foo/bar", gotCmds[0].Dir)
+	assert.Equal(t, []string{"git", "worktree", "remove"}, gotCmds[1].Args[0:3])
+	assert.Equal(t, "/foo/bar", gotCmds[1].Dir)
+}
