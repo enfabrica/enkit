c46449237ca2f325e4263097b2d7a889bf545d8a
Scott Minor <scott@enfabrica.net>
2021-10-08 16:55:03 +0000

    Addressed carlo's comments

--- lib/git/worktree.go
+++ lib/git/worktree.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os/exec"
-
-	"github.com/golang/glog"
 )
 
+// TempWorktree is a handle to a git worktree directory created with a
+// reference to an existing git repository.
 type TempWorktree struct {
-	repoPath string
+	repoPath     string
 	worktreePath string
 }
 
@@ -17,34 +17,50 @@ var runCommand = func(cmd *exec.Cmd) ([]byte, error) {
 	return cmd.Output()
 }
 
+// NewTempWorktree creates a worktree in a temporary directory for the git
+// repository at the specified path with the specified committish (commit hash,
+// short commit hash, branch name, etc.). It is designed to be used to
+// "checkout" a git repository to a particular point without affecting the main
+// worktree, which carries the risk of losing data/work and/or conflicting with
+// other processes.
+//
+// This function exists because go-git does not appear to have the ability to
+// generate git worktrees; only traverse them.
 func NewTempWorktree(repoPath string, committish string) (*TempWorktree, error) {
 	tmpDir, err := ioutil.TempDir("", "git_temp_worktree_*")
 	if err != nil {
-		return nil, fmt.Errorf("failed to create temp directory: %v", err)
+		return nil, fmt.Errorf("failed to create temp directory: %w", err)
 	}
 	cmd := exec.Command("git", "worktree", "add", tmpDir, committish)
 	cmd.Dir = repoPath
 	_, err = runCommand(cmd)
 	if err != nil {
-		return nil, fmt.Errorf("failed to construct temp worktree: %v", err)
+		return nil, fmt.Errorf("failed to construct temp worktree: %w", err)
 	}
 	return &TempWorktree{
-		repoPath: repoPath,
+		repoPath:     repoPath,
 		worktreePath: tmpDir,
 	}, nil
 }
 
+// Root returns the path to the root directory of the worktree.
 func (w *TempWorktree) Root() string {
 	return w.worktreePath
 }
 
+// Close deletes the worktree from disk and unregisters it from git.
+//
+// If a worktree is not closed, `git worktree list` will show the associated
+// directory.
+// If a worktree is deleted out-of-band of `git worktree remove`, `git worktree
+// list` will still show it as registered but prunable; the list can be cleaned
+// up with `git worktree prune`.
 func (w *TempWorktree) Close() error {
 	cmd := exec.Command("git", "worktree", "remove", w.worktreePath)
 	cmd.Dir = w.repoPath
 	_, err := runCommand(cmd)
 	if err != nil {
-		err = fmt.Errorf("failed to delete temp git worktree at %q: %v", w.worktreePath, err)
-		glog.Warning(err)
+		err = fmt.Errorf("failed to delete temp git worktree at %q: %w", w.worktreePath, err)
 		return err
 	}
 	return nil
--- lib/git/worktree_test.go
+++ lib/git/worktree_test.go
@@ -4,57 +4,27 @@ import (
 	"os/exec"
 	"testing"
 
-	"github.com/enfabrica/enkit/lib/errdiff"
-
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewTempWorktree(t *testing.T) {
-	testCases := []struct {
-		desc         string
-		committish   string
-		wantCmds     []*exec.Cmd
-		wantErr      string
-		wantCloseErr string
-	}{
-		{
-			desc:       "successfully creates temp worktree",
-			committish: "some_branch_name",
-			wantCmds: []*exec.Cmd{
-				&exec.Cmd{
-					Path: "git",
-					Args: []string{"worktree", "add", "foo", "some_branch_name"},
-					Dir:  "/foo/bar",
-				},
-				&exec.Cmd{
-					Path: "git",
-					Args: []string{"worktree", "remove", "foo"},
-					Dir:  "/foo/bar",
-				},
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.desc, func(t *testing.T) {
-			gotCmds := []*exec.Cmd{}
-			stubs := gostub.Stub(&runCommand, func(cmd *exec.Cmd) ([]byte, error) {
-				gotCmds = append(gotCmds, cmd)
-				return nil, nil
-			})
-			defer stubs.Reset()
+	gotCmds := []*exec.Cmd{}
+	stubs := gostub.Stub(&runCommand, func(cmd *exec.Cmd) ([]byte, error) {
+		gotCmds = append(gotCmds, cmd)
+		return nil, nil
+	})
+	defer stubs.Reset()
 
-			got, gotErr := NewTempWorktree("/foo/bar", tc.committish)
-			closeErr := got.Close()
+	got, gotErr := NewTempWorktree("/foo/bar", "some_branch_name")
+	closeErr := got.Close()
 
-			errdiff.Check(t, gotErr, tc.wantErr)
-			errdiff.Check(t, closeErr, tc.wantCloseErr)
-			assert.Equal(t, 2, len(gotCmds))
-			assert.Equal(t, []string{"git", "worktree", "add"}, gotCmds[0].Args[0:3])
-			assert.Equal(t, []string{"some_branch_name"}, gotCmds[0].Args[4:])
-			assert.Equal(t, "/foo/bar", gotCmds[0].Dir)
-			assert.Equal(t, []string{"git", "worktree", "remove"}, gotCmds[1].Args[0:3])
-			assert.Equal(t, "/foo/bar", gotCmds[1].Dir)
-		})
-	}
+	assert.NoError(t, gotErr)
+	assert.NoError(t, closeErr)
+	assert.Equal(t, 2, len(gotCmds))
+	assert.Equal(t, []string{"git", "worktree", "add"}, gotCmds[0].Args[0:3])
+	assert.Equal(t, []string{"some_branch_name"}, gotCmds[0].Args[4:])
+	assert.Equal(t, "/foo/bar", gotCmds[0].Dir)
+	assert.Equal(t, []string{"git", "worktree", "remove"}, gotCmds[1].Args[0:3])
+	assert.Equal(t, "/foo/bar", gotCmds[1].Dir)
 }
