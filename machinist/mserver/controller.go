c990be417b31ad02a5187197b063aaa68a5ec625
adam <adam@enfabrica.net>
2021-10-12 19:10:11 -0700

    add in property

--- machinist/mserver/controller.go
+++ machinist/mserver/controller.go
@@ -18,17 +18,18 @@ type Controller struct {
 
 	startUpFunc []func()
 
+	allRecordsRefreshRate time.Duration
 
 	State         *state.MachineController
 	stateFile     string
 	stateWriteTTL time.Duration
 
 	dnsServer *kdns.DnsServer
-	dnsPort   int
 	domains   []string
 }
+
 // Init is designed to run after all components have been started up before running itself as a server
-func (en *Controller) Init()  {
+func (en *Controller) Init() {
 	for _, m := range en.State.Machines {
 		en.addNodeToDns(m.Name, m.Ips, m.Tags)
 	}
