7dab1a6b8fa5f2361c725253eecff4f823ab11d8
adam <adam@enfabrica.net>
2021-10-20 13:22:01 -0700

    revert changes on workspace

--- bazel/linting/scripts/git.sh
+++ bazel/linting/scripts/git.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# This script gets the stable diff from the workspace command
 if [ -z "$1" ]; then
   echo "must present status file"
   exit 1
 fi
 line=$(grep STABLE_GIT_MASTER_DIFF "$1" | cut -d " "  -f2-)
+# this is specifically disabled because we want the side effect
 # shellcheck disable=SC2162
 read -a arr <<< "$line"
 for i in "${arr[@]}"
