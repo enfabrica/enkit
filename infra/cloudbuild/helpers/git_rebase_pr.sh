10dc7398cbd186eb5db74976ad965639c8864c42
Scott Minor <minor@minorhacks.com>
2021-10-22 10:14:28 -0700

    Add bazel-based presubmit
    This change adds a cloudbuild.yaml presubmit to enkit.
    
    This presubmit:
    * rebases the PR branch onto the latest master
    * detects affected targets between master and the tip of the PR branch
    * builds all affected targets
    * runs all affected tests
    
    This change does not include:
    
    * Filtering based on tags - we need some way to exclude tests from being
      run in presubmit, if they are expensive or flaky
    * Optimizations - getting to the build phase takes ~8.5 minutes, which
      is very long and could possibly be reduced.
    
    Tested: https://console.cloud.google.com/cloud-build/builds;region=global/ad6bf07b-7496-4d26-b61b-57a0908bdaf8;step=0?project=cloud-build-290921

---
+++ infra/cloudbuild/helpers/git_rebase_pr.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+# Rebase PR onto the tip of master.
+# Requires:
+# * Image with `git`
+# * Mounted `/root/.ssh` volume
+#
+# Example usage:
+#  - name: gcr.io/cloud-builders/git
+#    entrypoint: bash
+#    args:
+#      - -c
+#      - infra/cloudbuild/helpers/git_rebase_pr.sh
+#    volumes:
+#      - name: ssh
+#        path: /root/.ssh
+
+# Impersonate PR author when rebasing
+# Rebasing will reauthor commits, and as no author is configured by default,
+# git will complain and bail. Configure the author and email for this build
+# from the last commit, which should match the PR author, as long as PRs
+# don't have multiple collaborators. This way, anything that reads the commit
+# author name/email won't be bamboozled by e.g. a dummy value here.
+git config user.email "$(git log --format='%ae' -1)"
+git config user.name "$(git log --format='%an' -1)"
+
+
+# Give the PR head a local branch name
+# This branch name is referenced in future rebasing steps.
+git checkout -b github_pr
+
+# Fetch more commits of the history
+# We need to find a common ancestor between the PR commits and master in order
+# for the rebase to succeed.  The `deepen` value likely needs to be the maximum
+# of "number of commits in a PR" and "number of commits master is allowed to
+# move ahead by". Potential scalability problem here: master moves at a rate
+# proportional to the number of devs. So as more people join, we need to fetch
+# more master to find a common ancestor.
+#
+# Realistically, this probably becomes "number of commits master moves in time
+# period t" where t is something like one week, and then we take that position
+# that PRs must be rebased at least weekly if one wants presubmits to run
+# properly.
+git fetch --deepen=100
+
+# Rebase or error
+# TODO(scott): Add instructions for rebasing or a pointer to such instructions
+# in the error message.
+readonly COMMON_ANCESTOR="$(git merge-base origin/master github_pr)"
+git rebase "${COMMON_ANCESTOR}" github_pr --onto origin/master || { echo "
+********************************************************************************
+** Auto-rebase failure **                                                      *
+********************************************************************************
+* Presubmits rebase your PR onto the latest master before running, but this    *
+* has failed because your PR is too out-of-date. Please rebase your PR to pick *
+* updates from master and re-push.                                             *
+********************************************************************************
+"; /bin/false; }
