5156364698160f7a806c779d8adf3220fab2500e
Scott Minor <minor@minorhacks.com>
2021-12-02 16:26:27 -0800

    bestie: Implement logging BEP server
    This change implements the Build Event Protocol server in a minimal
    fashion, printing out all messages received on stdout in textproto
    format and acking them.
    
    Tested: Started server, ran `bazel build //bestie/...
    --bes_backend=grpc://localhost:6433` and saw messages printed on stdout;
    bazel exited with no error.
    
    Jira: INFRA-193

--- bestie/server/main.go
+++ bestie/server/main.go
@@ -2,29 +2,66 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 
-	hpb "github.com/enfabrica/enkit/bestie/proto"
 	"github.com/enfabrica/enkit/lib/server"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
+	//"google.golang.org/grpc/codes"
+	//"google.golang.org/grpc/status"
+	//"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+	//"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/encoding/prototext"
+	bpb "google.golang.org/genproto/googleapis/devtools/build/v1"
 )
 
-type GreeterService struct {
+type BuildEventService struct {
 }
 
-func (s *GreeterService) Greet(ctx context.Context, req *hpb.GreetRequest) (*hpb.GreetResponse, error) {
-	return &hpb.GreetResponse{
-		Greeting: fmt.Sprintf("Hello, %s!", req.GetName()),
-	}, nil
+func (s *BuildEventService) PublishLifecycleEvent(ctx context.Context, req *bpb.PublishLifecycleEventRequest) (*emptypb.Empty, error) {
+	fmt.Printf("%s\n\n", prototext.Format(req))
+	return &emptypb.Empty{}, nil
+}
+
+func (s *BuildEventService) PublishBuildToolEventStream(stream bpb.PublishBuildEvent_PublishBuildToolEventStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s\n\n", prototext.Format(req))
+		// TODO(scott): Print out Bazel BES message once protos are available
+		// if bazelEvent, ok := req.GetOrderedBuildEvent().GetEvent().GetEvent().(*bpb.BuildEvent_BazelEvent); ok {
+		// 	event := &bespb.BuildEvent{}
+		// 	if err := anypb.UnmarshalTo(bazelEvent.BazelEvent, event, proto.UnmarshalOptions{}); err != nil {
+		// 		return err
+		// 	}
+		// 	fmt.Printf("%s\n\n", prototext.Format(event))
+		// }
+
+		res := &bpb.PublishBuildToolEventStreamResponse{
+			StreamId: req.GetOrderedBuildEvent().StreamId,
+			SequenceNumber: req.GetOrderedBuildEvent().SequenceNumber,
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
 	grpcs := grpc.NewServer()
-	s := &GreeterService{}
-	hpb.RegisterGreeterServer(grpcs, s)
+	bpb.RegisterPublishBuildEventServer(grpcs, &BuildEventService{})
 
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
