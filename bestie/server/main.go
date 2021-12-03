8160be459a59f25e8173f37843950e61d62aec35
Scott Minor <minor@minorhacks.com>
2021-12-03 09:56:01 -0800

    Add CODEOWNERS for third_party

--- bestie/server/main.go
+++ bestie/server/main.go
@@ -8,14 +8,11 @@ import (
 	"net/http"
 
 	"github.com/enfabrica/enkit/lib/server"
+	_ "github.com/enfabrica/enkit/third_party/bazel/buildeventstream" // Allows prototext to automatically decode embedded messages
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
-	//"google.golang.org/grpc/codes"
-	//"google.golang.org/grpc/status"
-	//"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
-	//"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/encoding/prototext"
 	bpb "google.golang.org/genproto/googleapis/devtools/build/v1"
 )
@@ -24,7 +21,7 @@ type BuildEventService struct {
 }
 
 func (s *BuildEventService) PublishLifecycleEvent(ctx context.Context, req *bpb.PublishLifecycleEventRequest) (*emptypb.Empty, error) {
-	fmt.Printf("%s\n\n", prototext.Format(req))
+	fmt.Printf("# BEP LifecycleEvent message:\n%s\n\n", prototext.Format(req))
 	return &emptypb.Empty{}, nil
 }
 
@@ -38,15 +35,7 @@ func (s *BuildEventService) PublishBuildToolEventStream(stream bpb.PublishBuildE
 			return err
 		}
 
-		fmt.Printf("%s\n\n", prototext.Format(req))
-		// TODO(scott): Print out Bazel BES message once protos are available
-		// if bazelEvent, ok := req.GetOrderedBuildEvent().GetEvent().GetEvent().(*bpb.BuildEvent_BazelEvent); ok {
-		// 	event := &bespb.BuildEvent{}
-		// 	if err := anypb.UnmarshalTo(bazelEvent.BazelEvent, event, proto.UnmarshalOptions{}); err != nil {
-		// 		return err
-		// 	}
-		// 	fmt.Printf("%s\n\n", prototext.Format(event))
-		// }
+		fmt.Printf("# BEP BuildToolEvent message:\n%s\n\n", prototext.Format(req))
 
 		res := &bpb.PublishBuildToolEventStreamResponse{
 			StreamId: req.GetOrderedBuildEvent().StreamId,
