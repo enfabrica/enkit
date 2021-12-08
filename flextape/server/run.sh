4b7ad8c73fa1a48d00a42fef18ff73a78ad64804
Scott Minor <minor@minorhacks.com>
2021-12-07 17:58:05 -0800

    flextape: Add Docker deployment script (#354)
    This change contains a script with the Docker command used to bring up
    Flextape on build-00. This script can get deleted once flextape is
    migrated to the cluster.
    
    Tested: Ran this command on build-00; flextape is currently running.
    
    Jira: INFRA-252

---
+++ flextape/server/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PORT=8000
+
+docker run \
+  -d \
+  -t \
+  -p ${PORT}:${PORT} \
+  -e "PORT=${PORT}" \
+  --name=flextape \
+  --restart="always" \
+  "gcr.io/devops-284019/infra/flextape@sha256:ad2a1035790ff148ad434feee98b3cbb97c2406a48725c80a4d4217078d4213e"
