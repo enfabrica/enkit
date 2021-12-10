d02ecdd8e04c5a74f305a08e1479dcdcb658e0b3
Carlo Contavalli <carlo@enfabrica.net>
2021-12-09 23:18:35 -0800

    lib/token: add support for asymmetric encryption. (#337)
    This is adding in yet another encoder to allow encrypting
    data using private and public keys.
    
    Some of this code comes from astore, a next step could be
    to refactor the internal astore code to use the token library.

--- lib/token/asymmetric.go
+++ lib/token/asymmetric.go
@@ -2,9 +2,11 @@ package token
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"math/rand"
 
+	"golang.org/x/crypto/nacl/box"
 	"golang.org/x/crypto/nacl/sign"
 )
 
@@ -100,4 +102,137 @@ func (t *SigningEncoder) Decode(ctx context.Context, value []byte) (context.Cont
 		return ctx, nil, fmt.Errorf("signature did not match")
 	}
 	return ctx, data, nil
+}
+
+const AsymmetricKeyLength = 32
+
+type AsymmetricKey [AsymmetricKeyLength]byte
+
+func (k *AsymmetricKey) ToByte() *[AsymmetricKeyLength]byte {
+	return (*[AsymmetricKeyLength]byte)(k)
+}
+
+const AsymmetricNonceLength = 24
+
+type AsymmetricNonce [AsymmetricNonceLength]byte
+
+func (n *AsymmetricNonce) ToByte() *[AsymmetricNonceLength]byte {
+	return (*[AsymmetricNonceLength]byte)(n)
+}
+
+func AsymmetricKeyFromSlice(key []byte) (*AsymmetricKey, error) {
+	parsedAsymmetricKey := AsymmetricKey{}
+	if copy(parsedAsymmetricKey[:], key) != AsymmetricKeyLength || len(key) > AsymmetricKeyLength {
+		return nil, fmt.Errorf("invalid key length - must be %d bytes", AsymmetricKeyLength)
+	}
+	return &parsedAsymmetricKey, nil
+}
+
+func AsymmetricKeyFromString(key string) (*AsymmetricKey, error) {
+	return AsymmetricKeyFromSlice([]byte(key))
+}
+
+func AsymmetricKeyFromHex(key string) (*AsymmetricKey, error) {
+	slice, err := hex.DecodeString(key)
+	if err != nil {
+		return nil, err
+	}
+	return AsymmetricKeyFromSlice(slice)
+}
+
+func AsymmetricNonceFromSlice(nonce []byte) (*AsymmetricNonce, error) {
+	parsedAsymmetricNonce := AsymmetricNonce{}
+	if copy(parsedAsymmetricNonce[:], nonce) != AsymmetricNonceLength || len(nonce) > AsymmetricNonceLength {
+		return nil, fmt.Errorf("invalid nonce length - must be %d bytes", AsymmetricNonceLength)
+	}
+	return &parsedAsymmetricNonce, nil
+}
+
+// AsymmetricEncoder is a BinaryEncoder capable of encrypting data with a public key
+// and decoding it using a public and private key pair.
+//
+// AsymmetricEncoder is based on the naccl library, it is pretty much an interface
+// adapter around the OpenAnonymous and SealAnonymous functions.
+type AsymmetricEncoder struct {
+	rng       *rand.Rand
+	priv, pub *AsymmetricKey
+}
+
+type AsymmetricSetter func(*AsymmetricEncoder) error
+
+func UsePrivateKey(key *AsymmetricKey) AsymmetricSetter {
+	return func(be *AsymmetricEncoder) error {
+		be.priv = key
+		return nil
+	}
+}
+
+func UsePublicKey(key *AsymmetricKey) AsymmetricSetter {
+	return func(be *AsymmetricEncoder) error {
+		be.pub = key
+		return nil
+	}
+}
+
+func UseKeyPair(pub, priv *AsymmetricKey) AsymmetricSetter {
+	return func(be *AsymmetricEncoder) error {
+		if err := UsePublicKey(pub)(be); err != nil {
+			return err
+		}
+		return UsePrivateKey(priv)(be)
+	}
+}
+
+// GenerateAsymmetricKeys generates a public key and private key.
+func GenerateAsymmetricKeys(rng *rand.Rand) (*AsymmetricKey, *AsymmetricKey, error) {
+	pub, priv, err := box.GenerateKey(rng)
+	return (*AsymmetricKey)(pub), (*AsymmetricKey)(priv), err
+}
+
+// Creates a new random public and private key, or return error.
+func WithGeneratedAsymmetricKey() AsymmetricSetter {
+	return func(be *AsymmetricEncoder) error {
+		pub, priv, err := GenerateAsymmetricKeys(be.rng)
+		if err != nil {
+			return err
+		}
+		return UseKeyPair(pub, priv)(be)
+	}
+}
+
+func NewAsymmetricEncoder(rng *rand.Rand, setters ...AsymmetricSetter) (*AsymmetricEncoder, error) {
+	be := &AsymmetricEncoder{rng: rng}
+	for _, setter := range setters {
+		if err := setter(be); err != nil {
+			return nil, err
+		}
+	}
+
+	if be.pub == nil {
+		return nil, fmt.Errorf("a public key MUST always be provided")
+	}
+	return be, nil
+}
+
+func (t *AsymmetricEncoder) Encode(data []byte) ([]byte, error) {
+	return box.SealAnonymous(nil, data, t.pub.ToByte(), t.rng)
+}
+
+func (t *AsymmetricEncoder) Decode(ctx context.Context, ciphertext []byte) (context.Context, []byte, error) {
+	if t.priv == nil {
+		return ctx, nil, fmt.Errorf("a private key MUST be provided to decode the data")
+	}
+	original, ok := box.OpenAnonymous(nil, ciphertext, t.pub.ToByte(), t.priv.ToByte())
+	if !ok {
+		return ctx, nil, fmt.Errorf("error deciphering asymmetric data")
+	}
+	return ctx, original, nil
+}
+
+func (t *AsymmetricEncoder) PrivateKey() *AsymmetricKey {
+	return t.priv
+}
+
+func (t *AsymmetricEncoder) PublicKey() *AsymmetricKey {
+	return t.pub
 }
