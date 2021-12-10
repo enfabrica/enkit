5bff505c9b93f927e9227c53f4d08ef65b19cf6a
Scott Minor <scott@enfabrica.net>
2021-12-10 16:43:54 +0000

    flextape: Update image version
    Tested: Deployed to build-00
    
    Jira: INFRA-274

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:ad2a1035790ff148ad434feee98b3cbb97c2406a48725c80a4d4217078d4213e"
+  "gcr.io/devops-284019/infra/flextape@sha256:122c6fba315ebb13bf3fc43c536be08e53a5e9e4d79128eebe2f64fdc22ffea4"
