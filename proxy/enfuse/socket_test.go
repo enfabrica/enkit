81481acbac39e33c50aa6beaa2dd551db4e277fa
drunkengranite <drunkengranite@gmail.com>
2021-12-10 15:56:19 -0800

    refactor testing

--- proxy/enfuse/socket_test.go
+++ proxy/enfuse/socket_test.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"github.com/enfabrica/enkit/lib/knetwork"
 	"github.com/enfabrica/enkit/proxy/enfuse"
+	"github.com/enfabrica/enkit/proxy/enfuse/testserver"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
 	"io"
-	"net"
-	"net/http"
-	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -39,8 +37,7 @@ var testConnections []*testClientConn = []*testClientConn{
 }
 
 func TestSocketShim(t *testing.T) {
-	m := creatBasicServer(t)
-	s := httptest.NewServer(m)
+	s := testserver.NewWebSocketBasicClientServer(t)
 	defer s.Close()
 
 	// Dial all websocket clients to the httptest.Server
@@ -58,9 +55,8 @@ func TestSocketShim(t *testing.T) {
 	serverWebConn, _, err := websocket.DefaultDialer.Dial(url+"/server", nil)
 	assert.NoError(t, err)
 	serverNetLis, _ := knetwork.AllocatePort()
+	go testserver.WriteHellosToListener(t, serverNetLis)
 
-	// since normally this net.Listener is handled by the recv app itself, we implement a basic echo tcp server
-	go handleServerLis(t, serverNetLis)
 	// this will now forward connections to the net.Listener
 	serverShim := enfuse.NewWebsocketTCPShim(enfuse.DefaultPayloadStrategy, serverNetLis, serverWebConn)
 	defer serverShim.Close()
@@ -84,76 +80,4 @@ func TestSocketShim(t *testing.T) {
 		assert.Equal(t, io.EOF, err)
 		assert.Equal(t, fmt.Sprintf("hello %s", c.Name), string(buf[:numBytesRead]))
 	}
-}
-
-// handleServerLis just spawns a new handleNewServerConn for every new connection
-func handleServerLis(t *testing.T, l net.Listener) {
-	for {
-		c, err := l.Accept()
-		assert.NoError(t, err)
-		go handleNewServerConn(t, c)
-	}
-}
-
-// handleNewServerConn accepts the new client webConn and then just writes back "hello <initial payload>"
-func handleNewServerConn(t *testing.T, c net.Conn) {
-	for {
-		buf := make([]byte, 1024)
-		_, err := c.Read(buf)
-		assert.NoError(t, err)
-		retBytes := append([]byte("hello "), buf...)
-		_, err = c.Write(retBytes)
-		assert.NoError(t, err)
-	}
-}
-
-// creatBasicServer is a ruddy implementation of a redirect server. It's *really* basic and just serves as a conn forwarder.
-func creatBasicServer(t *testing.T) *http.ServeMux {
-	upg := websocket.Upgrader{}
-	pool := enfuse.NewPool(enfuse.DefaultPayloadStrategy)
-	m := http.NewServeMux()
-	m.HandleFunc("/client", func(writer http.ResponseWriter, request *http.Request) {
-		webConn, err := upg.Upgrade(writer, request, nil)
-		if err != nil {
-			http.Error(writer, "internal test error", 500)
-			return
-		}
-		for {
-			m, t, err := webConn.ReadMessage()
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			if err := pool.WriteWebsocketServer(m, t, webConn); err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-		}
-	})
-	m.HandleFunc("/server", func(writer http.ResponseWriter, request *http.Request) {
-		webConn, err := upg.Upgrade(writer, request, nil)
-		if err != nil {
-			http.Error(writer, "internal test error", 500)
-			return
-		}
-		if err := pool.SetServer(webConn); err != nil {
-			http.Error(writer, "server set err ", 500)
-			return
-		}
-		for {
-			t, payload, err := webConn.ReadMessage()
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			clientConn := pool.Fetch(payload)
-			if clientConn != nil {
-				if err := clientConn.WriteMessage(t, payload); err != nil {
-					fmt.Println(err.Error())
-					return
-				}
-			}
-		}
-	})
-	return m
 }
--- proxy/enfuse/websocket_test.go
+++ proxy/enfuse/websocket_test.go
@@ -1,64 +1,95 @@
 package enfuse_test
 
 import (
-	"fmt"
 	"github.com/enfabrica/enkit/proxy/enfuse"
+	"github.com/enfabrica/enkit/proxy/enfuse/testserver"
 	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 )
 
-func makeEchoServer(t *testing.T) *httptest.Server {
-	upg := websocket.Upgrader{}
-	m := http.NewServeMux()
-	m.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
-		webConn, err := upg.Upgrade(writer, request, nil)
-		if err != nil {
-			assert.NoError(t, err)
-			http.Error(writer, "internal test error", 500)
-			return
-		}
-		for {
-			m, data, err := webConn.ReadMessage()
-			if err != nil {
-				fmt.Println(err.Error())
-				continue
-			}
-			assert.NoError(t, webConn.WriteMessage(m, append([]byte("hello"), data...)))
-		}
-	})
-	return httptest.NewServer(m)
+func TestBasicPoolSet(t *testing.T) {
+	p := enfuse.NewPool(enfuse.DefaultPayloadStrategy)
+	assert.False(t, p.ServerPresent())
+	server1 := &websocket.Conn{}
+	assert.NoError(t, p.SetServer(server1))
+	assert.True(t, p.ServerPresent())
 }
 
-func TestSanityPool(t *testing.T) {
+func TestFetchReturnNilWithNoClients(t *testing.T) {
 	p := enfuse.NewPool(enfuse.DefaultPayloadStrategy)
-	uidLen, factoryUIDFunc := enfuse.DefaultPayloadStrategy()
+	uidLen, _ := enfuse.DefaultPayloadStrategy()
+	assert.Nil(t, p.Fetch(make([]byte, uidLen)))
+}
+
+func TestWritesToServer(t *testing.T) {
+	strategy := testserver.DoubleDigitPayloadStrategy
+	p := enfuse.NewPool(strategy)
+	uidLen, _ := strategy()
+
 	assert.Nil(t, p.Fetch(make([]byte, uidLen)))
 	assert.False(t, p.ServerPresent())
 
-	s := makeEchoServer(t)
+	var recvMessages []string
+	mu := sync.Mutex{}
+	s := testserver.NewWebsocketCounterServer(t, func(input []byte) {
+		mu.Lock()
+		recvMessages = append(recvMessages, string(input))
+		mu.Unlock()
+	})
 	defer s.Close()
 
 	url := strings.Replace(s.URL, "http", "ws", -1)
-	serverConn, _, err := websocket.DefaultDialer.Dial(url+"/echo", nil)
-	assert.NoError(t, err)
+	serverConn, _, err := websocket.DefaultDialer.Dial(url, nil)
 
+	assert.NoError(t, err)
 	assert.NoError(t, p.SetServer(serverConn))
+
 	assert.True(t, p.ServerPresent())
-	go func() {
-		for _ = range make([]int, 100){
-			go assert.NoError(t, p.SetServer(serverConn))
-			go assert.True(t, p.ServerPresent())
-		}
-	}()
-	time.Sleep(2 * time.Second)
+	messages := []string{"winnie", "piglet", "eeyore", "tigger"}
+	for _, message := range messages {
+		cc, _, err := websocket.DefaultDialer.Dial(url, nil)
+		assert.NoError(t, err)
+		err = p.WriteWebsocketServer(websocket.BinaryMessage, []byte(message), enfuse.NewWebsocketLock(cc))
+		assert.NoError(t, err)
+	}
+	// give time for cpu cycles
+	time.Sleep(200 * time.Millisecond)
+	// These locks look silly, but it's to prevent race condition detecion with go's test suites,
+	// which is important to be on for the main package
+	mu.Lock()
+	assert.ElementsMatch(t, messages, recvMessages)
+	mu.Unlock()
+}
 
-	uid, err := factoryUIDFunc()
+
+func TestClientFetch(t *testing.T) {
+	s := testserver.NewWebsocketCounterServer(t, func(input []byte) {})
+	defer s.Close()
+	url := strings.Replace(s.URL, "http", "ws", -1)
+	strategy := testserver.DoubleDigitPayloadStrategy
+	p := enfuse.NewPool(strategy)
+	serverConn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	assert.NoError(t, err)
+	assert.NoError(t, p.SetServer(serverConn))
 
-	assert.NoError(t, p.WriteWebsocketServer(websocket.BinaryMessage, append(uid, []byte("hello")...), serverConn))
+	clientConns := make([]*enfuse.WebsocketLocker, 5)
+	messages := []string{"winnie", "piglet", "eeyore", "tigger"}
+
+	for i, message := range messages {
+		cc, _, err := websocket.DefaultDialer.Dial(url, nil)
+		assert.NoError(t, err)
+		ccLocker := enfuse.NewWebsocketLock(cc)
+		err = p.WriteWebsocketServer(websocket.BinaryMessage, []byte(message), ccLocker)
+		assert.NoError(t, err)
+		clientConns[i] = ccLocker
+	}
+	// give time for cpu cycles
+	time.Sleep(200 * time.Millisecond)
+	for i, message := range messages {
+		assert.Equal(t, clientConns[i], p.Fetch([]byte(message)))
+	}
 }
