c262f51c46ef5c42a99521b1dd7b5340627a7de1
Scott Minor <scott@enfabrica.net>
2021-12-13 12:02:51 -0800

    flextape: Update to pull in LicensesStatus improvements (#367)
    This change bumps the flextape image hash used, and changes the default
    tag used when pushing images from "testing" to "latest".
    
    Tested: Currently running on `build-00`
    
    Jira: INFRA-287

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:122c6fba315ebb13bf3fc43c536be08e53a5e9e4d79128eebe2f64fdc22ffea4"
+  "gcr.io/devops-284019/infra/flextape@sha256:cdb12098e1f673f36ee4a82ae48f92f276e9fa18068b1080e0cb887e68a64636"
