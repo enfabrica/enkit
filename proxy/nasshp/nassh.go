0c2b04a7273c67f7c5e08c2b09ca163cc0bc366b
Carlo Contavalli <carlo@enfabrica.net>
2022-01-15 08:50:04 -0800

    proxy/: refactor for modularity and testing. (#383)
    The enkit proxy creates a MUX that is able to dispatch to an
    http proxy, or to the nassh tunnel handling (which happens to
    use websockets and GET/POST requests via HTTP).
    
    Creation of this MUX was performed directly from main, together
    with a bunch of flags/configuration parsing.
    
    Among other things, this made testing harder.
    
    In this PR:
    - break out code out of main() into a dedicated object, under
      enproxy/. This object follows the common conventions in the
      enkit/ repo, allowing direct initialization via flags or via
      simple modifiers/functors.
    - add unit test covering the basic functionality of the proxy
      as well as ability to establish tunnels.
    
    Note that this PR is still in preparation for follow up PRs:
    THERE WERE NO BEHAVIORAL CHANGES IN THIS PR. Tried to minimize
    code changes as much as possible, outside those changes necessary
    to break the code out.
    
    Follow up PRs will change a few behavioral things. Specifically:
    - add functionality to only test the config file
      (without loading the proxy)
    - metrics for prometheus
    - a few cleanups (eg, use a single mux).
    
    But this break down is a precursor to those changes.

--- proxy/nasshp/nassh.go
+++ proxy/nasshp/nassh.go
@@ -43,6 +43,10 @@ type NasshProxy struct {
 	connections sync.Map
 }
 
+func (np *NasshProxy) RelayHost() string {
+	return np.relayHost
+}
+
 type options struct {
 	bufferSize       int
 	symmetricSetters []token.SymmetricSetter
@@ -148,7 +152,7 @@ func FromFlags(fl *Flags) Modifier {
 		if len(fl.SymmetricKey) == 0 {
 			key, err := token.GenerateSymmetricKey(o.rng, 0)
 			if err != nil {
-				return fmt.Errorf("the world is about to end, even random nubmer generators are failing - %w", err)
+				return fmt.Errorf("the world is about to end, even random number generators are failing - %w", err)
 			}
 			fl.SymmetricKey = key
 		}
@@ -183,6 +187,14 @@ func WithTimeouts(timeouts *Timeouts) Modifier {
 	}
 }
 
+// New creates a new instance of a nasshp tunnel protocol.
+//
+// rng MUST be a secure random number generator, use github.com/enfabrica/lib/srand
+// in case of doubt to create one.
+// authenticator is optional, can be left to nil to disable authentication.
+//
+// mods MUST either contain FromFlags, to initialize all the nassh parameters from
+// command line flags, or it MUST provide a symmetric key with nasshp.WithSymmetricOptions.
 func New(rng *rand.Rand, authenticator oauth.Authenticate, mods ...Modifier) (*NasshProxy, error) {
 	o := &options{rng: rng, bufferSize: 8192}
 	np := &NasshProxy{
@@ -207,7 +219,7 @@ func New(rng *rand.Rand, authenticator oauth.Authenticate, mods ...Modifier) (*N
 	if np.encoder == nil {
 		be, err := token.NewSymmetricEncoder(rng, o.symmetricSetters...)
 		if err != nil {
-			return nil, fmt.Errorf("error setting up symmetric encryption: %w", err)
+			return nil, fmt.Errorf("nassh - error setting up symmetric encryption: %w", err)
 		}
 
 		ue := token.NewBase64UrlEncoder()
