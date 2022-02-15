16df3aedfe7001286c120baccd1b035286d76138
Carlo Contavalli <carlo@enfabrica.net>
2022-02-15 13:54:48 +0000

    flextape: add docstrings to queue.go, fix minor bug in Position()

--- flextape/service/queue.go
+++ flextape/service/queue.go
@@ -11,8 +11,14 @@ import (
 //   implementation of invocation prioritization policies.
 // - It maintains per-entry state (QueueID) so it's easy to tell
 //   the position of each invocation by just looking at that value.
+//
+// invocationQueue is NOT thread safe. The caller must ensure that
+// only a single thread can access the queue at any given time.
 type invocationQueue []*invocation
 
+// Enqueue adds an item to the queue.
+//
+// During addition, an appropriate QueueID is assigned.
 func (iq *invocationQueue) Enqueue(x *invocation) Position {
 	position := Position(len(*iq))
 	offset := QueueID(1)
@@ -25,6 +31,9 @@ func (iq *invocationQueue) Enqueue(x *invocation) Position {
 	return position
 }
 
+// Dequeue removes an item from the queue.
+//
+// When an item is removed from the queue, its QueueID is reset to zero.
 func (iq *invocationQueue) Dequeue() *invocation {
 	if len(*iq) <= 0 {
 		return nil
@@ -36,10 +45,12 @@ func (iq *invocationQueue) Dequeue() *invocation {
 	return retval
 }
 
+// Len returns the length of the queue.
 func (iq *invocationQueue) Len() int {
 	return len(*iq)
 }
 
+// Swap swaps two elements of the queue, taking care of updating the QueueID.
 func (iq *invocationQueue) Swap(i, j int) {
 	(*iq)[i].QueueID, (*iq)[j].QueueID = (*iq)[j].QueueID, (*iq)[i].QueueID
 	(*iq)[i], (*iq)[j] = (*iq)[j], (*iq)[i]
@@ -57,6 +68,12 @@ type Filter func(pos Position, inv *invocation) bool
 // should continue, false if it should stop.
 type Walker func(pos Position, inv *invocation) bool
 
+// Filter removes the elements based on the filter callback provided.
+//
+// If the filter returns true, the element is removed, otherwise it is preserved.
+//
+// Returns the number of elements removed, and update the QueueID of elements
+// as necessary.
 func (iq *invocationQueue) Filter(filter Filter) int {
 	newQueue := []*invocation{}
 	count := 0
@@ -73,6 +90,12 @@ func (iq *invocationQueue) Filter(filter Filter) int {
 	return count
 }
 
+// Walk invokes the walker function for each element of the queue.
+//
+// If the walker returns false, the walk is interrupted.
+//
+// Returns the invocation and position of the element the walk was
+// interrupted on, or (nil, 0).
 func (iq *invocationQueue) Walk(walker Walker) (*invocation, Position) {
 	for posx, inv := range *iq {
 		pos := Position(posx + 1)
@@ -83,6 +106,10 @@ func (iq *invocationQueue) Walk(walker Walker) (*invocation, Position) {
 	return nil, 0
 }
 
+// Forget removes the specified invocation ID from the queue.
+//
+// Returns the invocation removed, or nil if it was not found.
+// As the element is removed, QueueID is reset to 0.
 func (iq *invocationQueue) Forget(invID string) *invocation {
 	var retinv *invocation
 	iq.Filter(func(pos Position, inv *invocation) bool {
@@ -96,6 +123,9 @@ func (iq *invocationQueue) Forget(invID string) *invocation {
 	return retinv
 }
 
+// Get returns the invocation and position corresponding to an invocation id.
+//
+// In case the invocation id is not found, (nil, 0) is returned instead.
 func (iq *invocationQueue) Get(invID string) (*invocation, Position) {
 	return iq.Walk(func(pos Position, inv *invocation) bool {
 		return inv.ID != invID
@@ -114,6 +144,7 @@ type sorter struct {
 	sort Sorter
 }
 
+// Less implements the Sort.Interface Less method.
 func (s *sorter) Less(i, j int) bool {
 	return s.sort((*s.invocationQueue)[i], (*s.invocationQueue)[j])
 }
@@ -127,9 +158,10 @@ func (iq *invocationQueue) Sort(p Sorter) {
 }
 
 // Position returns the Position of an invocation in the queue.
+//
 // If the invocation is not queued, returns the 0 Position.
 func (iq *invocationQueue) Position(inv *invocation) Position {
-	if len(*iq) <= 0 {
+	if len(*iq) <= 0 || inv.QueueID == 0 {
 		return 0
 	}
 
