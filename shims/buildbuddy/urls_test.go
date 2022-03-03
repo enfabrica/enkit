eb133cc4e58c38f4e1d884a95e799dce3d109ab9
drunkengranite <drunkengranite@gmail.com>
2022-03-03 01:29:37 -0800

    remove vestigial stuff

--- shims/buildbuddy/urls_test.go
+++ shims/buildbuddy/urls_test.go
@@ -10,11 +10,6 @@ import (
 	"testing"
 )
 
-type UrlReqResp struct {
-	Pre  string
-	Post string
-}
-
 var TestUrls = []string{
 	"/file/download?filename=model%2Ffunctional%2Fsfa-model%2Flibmodel-queues-memregions.a&bytestream_url=bytestream%3A%2F%2Fbuild.local.enfabrica.net%3A8000%2Fblobs%2Ff16674c20f3a871becfd1e44d343cf2e7afd6fdbb2fc0be5bb4aa58de497ef5d%2F538362&invocation_id=f69b6189-c598-4241-862a-e52125dd12e6",
 	"/file/download?filename=hw%2Fcommon%2Fdv%2Fbase%2Fdv_utils_pkg_elab&bytestream_url=bytestream%3A%2F%2Fbuild.local.enfabrica.net%3A8000%2Fblobs%2Fd083637acce47a592e3a99f329603b8f80d6d96b173ba08997744fc53df081ce%2F3365&invocation_id=d2f85a71-ddff-46f8-bb2b-71db65f8e3c9",
