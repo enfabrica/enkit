d73673d84e978133f0ad13762a825dbbca8ffe9a
Adam Ahrens <drunkengranite@gmail.com>
2022-03-07 12:45:14 -0800

    infra: create buildbuddy shim (#489)
    This is a shim designed to be routed at L7, and captures any /file/download url and rewrites it to have the bytestream url be the intra-cluster url name of the storage section of buildbarn. It then forwards to any url of choice (usually the existing buildbuddy service)
    
    As far as integration tests go, this is installed on staging currently. Seems to work nicely.
    
    jira: INFRA-683

---
+++ shims/buildbuddy/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/enfabrica/enkit/shims/buildbuddy"
+	"github.com/spf13/cobra"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	var redirectUrlTo string
+	var byteStreamHost string
+	var servePrefix string
+	var port int
+
+	c := cobra.Command{
+		Use: "serve",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			u, err := url.Parse(redirectUrlTo)
+			if err != nil {
+				return fmt.Errorf("err with parsing %s %v", redirectUrlTo, err)
+			}
+			parsedByteStreamHost, err := url.Parse(byteStreamHost)
+			if err != nil {
+				return fmt.Errorf("err with parsing bytestreamhost %s %v", byteStreamHost, err)
+			}
+			fmt.Printf("Redirecting to %s \n", u.String())
+			fmt.Printf("BytestreamHost is to %s \n", parsedByteStreamHost.Host)
+			proxy := httputil.NewSingleHostReverseProxy(u)
+			h := buildbuddy.NewHandler(servePrefix, parsedByteStreamHost.Host, proxy)
+			fmt.Println("listening and serving with cors")
+			return http.ListenAndServe(fmt.Sprintf(":%d", port), h)
+		},
+	}
+	c.Flags().StringVar(&servePrefix, "prefix", "/file/download", "the prefix of the http handler")
+	c.Flags().StringVar(&redirectUrlTo, "redirect-url", "buildbarn-buildbuddy-svc.buildbarn:8080", "the url of which to reverse proxy to")
+	c.Flags().StringVar(&byteStreamHost, "bytestream-host", "buildbarn-browser-svc.buildbarn", "the new host of the byte stream url")
+	c.Flags().IntVar(&port, "port", 8080, "port to serve on")
+
+	log.Fatal(c.Execute())
+}
