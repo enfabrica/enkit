9044560da98d079a3122921fc885dc3fea8c91ce
Peter Hyvonen <pete@enfabrica.net>
2022-03-24 21:18:29 -0700

    new sha

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:5f7ec2ff1e8aa34a0acd58691947cbc4ed7a9ceab06807e7d03e233dff1524dd"
+  "gcr.io/devops-284019/infra/flextape@sha256:cfcbab46556f1048d10c4fd0994c63d6903726f2844d09bf294d5612d2f57b9c"
