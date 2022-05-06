db4446192c21773f56bae31ed3621159d62d576f
Brian Huynh <bbhuynh@enfabrica.net>
2022-05-06 04:50:48 +0000

    Update flextape hash

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:b24caea0e6f08c22b24f50a1b7676c507bf4dbe3a3d07629bbbf0a282db18418"
+  "gcr.io/devops-284019/infra/flextape@sha256:a3535d2e50f72496a1cb03ce12e53c9fd945f9473d30f16c9cf00898d50d3d08"
