eddac9c091c0978c08e473172fe5d21f0c8c36ee
Scott Minor <scott@enfabrica.net>
2022-05-09 13:37:20 -0700

    enkit ssh: Add --proxy-map arg (#569)
    This change adds a `--proxy-map` argument to `enkit ssh`, allowing one
    to map bits of a SSH target address to its corresponding proxy. This
    will allow us to configure e.g.:
    
    ```
    --proxy-map=.foo=https://foo.example.com,.bar=https://bar.example.com
    ```
    
    in the enkit kconfig, so that a bare `enkit ssh user@machine-1.bar` will
    resolve to the (potentially non-default) proxy https://bar.example.com.
    
    This is done by matching all of the non-flag SSH args against all
    entries in `--proxy-map` via substring lookup until a match is found; if
    no match is found, the proxy named by `--proxy` is used as a fallback.
    
    This logic is likely to work most of the time but fall on its face for
    unusual invocations of `enkit ssh`, so proxy decisions are logged at at
    least the INFO level.
    
    Tested:
    * Added unit tests
    * Pushed https://github.com/enfabrica/internal/pull/4917 as staging config, tested machines
       in the three different areas listed
    
    Jira: INFRA-868

---
+++ proxy/ptunnel/commands/ssh_test.go
@@ -0,0 +1,115 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/enfabrica/enkit/lib/client"
+	"github.com/enfabrica/enkit/lib/errdiff"
+	"github.com/enfabrica/enkit/lib/logger"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSSHParseFlags(t *testing.T) {
+	testCases := []struct {
+		desc         string
+		proxyList    []string
+		wantProxyMap []proxyMapping
+		wantErr      string
+	}{
+		{
+			desc:         "no entries",
+			proxyList:    nil,
+			wantProxyMap: nil,
+		},
+		{
+			desc:      "one entry",
+			proxyList: []string{".foo=https://foo.example.com./"},
+			wantProxyMap: []proxyMapping{
+				proxyMapping{substring: ".foo", proxy: "https://foo.example.com./"},
+			},
+		},
+		{
+			desc:      "multiple entries",
+			proxyList: []string{".foo=https://foo.example.com./", ".bar=https://bar.example.com./"},
+			wantProxyMap: []proxyMapping{
+				proxyMapping{substring: ".foo", proxy: "https://foo.example.com./"},
+				proxyMapping{substring: ".bar", proxy: "https://bar.example.com./"},
+			},
+		},
+		{
+			desc:      "parse error",
+			proxyList: []string{".foo:https://foo.example.com./"},
+			wantErr:   "not a valid proxy mapping",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ssh := &SSH{
+				proxyList: tc.proxyList,
+			}
+
+			gotErr := ssh.parseFlags(nil, nil)
+
+			errdiff.Check(t, gotErr, tc.wantErr)
+			if gotErr != nil {
+				return
+			}
+			assert.Equal(t, ssh.ProxyMap, tc.wantProxyMap)
+		})
+	}
+}
+
+var exampleProxyMap = []proxyMapping{
+	proxyMapping{substring: ".foo", proxy: "https://foo.example.com./"},
+	proxyMapping{substring: ".bar", proxy: "https://bar.example.com./"},
+	proxyMapping{substring: ".baz", proxy: "https://baz.example.com./"},
+}
+
+func TestSSHChooseProxy(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		proxy     string
+		proxyMap  []proxyMapping
+		sshArgs   []string // One of these is expected to be the target ($USER@$MACHINE)
+		wantProxy string   // Flag in the form ` --proxy=$PROXY_URL`
+	}{
+		{
+			desc:      "target in proxy map",
+			proxy:     "https://default.example.com./",
+			proxyMap:  exampleProxyMap,
+			sshArgs:   []string{"user@machine-1.bar"},
+			wantProxy: " --proxy=https://bar.example.com./",
+		},
+		{
+			desc:      "target not in proxy map with default proxy",
+			proxy:     "https://default.example.com./",
+			proxyMap:  exampleProxyMap,
+			sshArgs:   []string{"user@machine-1.quux"},
+			wantProxy: " --proxy=https://default.example.com./",
+		},
+		{
+			desc:      "target not in proxy map with no default proxy",
+			proxyMap:  exampleProxyMap,
+			sshArgs:   []string{"user@machine-1.quux"},
+			wantProxy: "",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			ssh := &SSH{
+				Proxy:    tc.proxy,
+				ProxyMap: tc.proxyMap,
+				BaseFlags: &client.BaseFlags{
+					Log: &logger.Proxy{
+						Logger: logger.Nil,
+					},
+				},
+			}
+
+			got := ssh.chooseProxy(tc.sshArgs)
+
+			assert.Equal(t, got, tc.wantProxy)
+		})
+	}
+}
