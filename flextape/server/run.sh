c5e2a9bbad5060d74fec710255ca0ef14a782913
Brian Huynh <bbhuynh@enfabrica.net>
2022-06-09 04:32:25 +0000

    Update flextape hash (#655)
    JIRA: INFRA-1197
    
    Testing:
    Deployed on infra-00

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:4b17812214790cfd1633355cfcd1a13f189463ae738e177f73d84e9e440dc2af"
+  "gcr.io/devops-284019/infra/flextape@sha256:a3af8d3ddf84d666a064eb4826d2aa8bb8dba42c9ddc1734192a0c8523f64ad4"
