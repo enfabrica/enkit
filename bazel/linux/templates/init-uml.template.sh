58c84f1519659a97bb1e4539807cdd90a8cb92cb
george-enf <george@enfabrica.net>
2022-06-13 09:10:31 -0700

    bazel/linux: split init.template.sh (#659)
    We plan to add features in QEMU runs that are not compatible with UML.
    Split the init.template.sh to simplify this process.
    
    Signed-off-by: George Prekas <george@enfabrica.net>

---
+++ bazel/linux/templates/init-uml.template.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo ========= {message} - {target} ==========
+trap "poweroff -f" EXIT
+
+# Find the "root" where the package was mounted.
+path="$0"
+relpath="{relpath}"
+dir="${path%%$relpath}"
+test "$dir" == "$path" || cd "$dir"
+
+set -e
+{commands}
