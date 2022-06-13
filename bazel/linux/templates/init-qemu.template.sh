enfabrica/enkit

commit 65d5933499d18ef02b0d6e5a297d0ce6d70001fe

bazel/linux: propagate the exit status outside the emulator (#638)
Success or failure inside the emulator is propagated to the exit status
of bazel run. The init script that executes in the emulator stores its
exit status in a temporary file. The script that executes the emulator
adjusts its own exit status based on this file.

Jira: https://enfabrica.atlassian.net/browse/SF-165
Signed-off-by: George Prekas <george@enfabrica.net>

bazel/linux/templates/init-uml.template.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 echo ========= {message} - {target} ==========
-trap "poweroff -f" EXIT
+on_exit() {
+    echo $? > "/tmp/output_dir/exit_status_file" || true
+    poweroff -f
+}
+trap on_exit EXIT
 
 # Find the "root" where the package was mounted.
 path="$0"

bazel/linux/templates/runner.template.sh
@@ -161,6 +161,11 @@ test "$estatus" == 0 || {
     echo 1>&2 "===== emulator exited with non zero status - check logs above ===="
     exit "$estatus"
 }
+test -f "$OUTPUT_DIR/exit_status_file" && estatus=$(< "$OUTPUT_DIR/exit_status_file")
+test "$estatus" == 0 || {
+    echo 1>&2 "===== a program in the emulator exited with non zero status - check logs above ===="
+    exit "$estatus"
+}
 test -z "$INTERACTIVE" -a -z "$SINGLE" || exit "$estatus"
 
 echo 1>&2 "===== emulator exited - now running checkers (if any) ===="

commit 53cb3b1c5e014e2d5618cfb8e485112b34dd656b

bazel/linux: fix propagation of exit status
We support propagating the exit status only when using QEMU. By mistake,
we updated the wrong init.template.sh.

Fixes: 65d5933499d1 ("bazel/linux: propagate the exit status outside the emulator")
Signed-off-by: George Prekas <george@enfabrica.net>

bazel/linux/templates/init-qemu.template.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 echo ========= {message} - {target} ==========
-trap "poweroff -f" EXIT
+on_exit() {
+    echo $? > "/tmp/output_dir/exit_status_file" || true
+    poweroff -f
+}
+trap on_exit EXIT
 
 # Find the "root" where the package was mounted.
 path="$0"

bazel/linux/templates/init-uml.template.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 echo ========= {message} - {target} ==========
-on_exit() {
-    echo $? > "/tmp/output_dir/exit_status_file" || true
-    poweroff -f
-}
-trap on_exit EXIT
+trap "poweroff -f" EXIT
 
 # Find the "root" where the package was mounted.
 path="$0"