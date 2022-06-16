enfabrica/enkit

commit dbf665348d4f8b7ca6fc363bff175e6f3488252e

kbuild/v2: Update scripts to build/publish UML kernel artifacts (#577)
* kbuild/v2: Update scripts to build/publish UML kernel artifacts

This patch updates the kbuild scripts to also build and publish the
UML kernel artifacts.

The main runner script is now called "kpub-astore" and has been
reworked to take command line arguments.

This patch also updates the README to describe how to use the script
and what the scripts do.

Fixes: INFRA-952

Signed-off-by: Curt Brune <curt@enfabrica.net>

* kbuild/v2: Update README with production example

Put an example of how to run the script for production at the top.

Signed-off-by: Curt Brune <curt@enfabrica.net>

kbuild/v2/template/install-uml-bazel.sh
@@ -1,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+BASE_DIR="$(dirname $(realpath $0))"
+
+# These variables are templated
+KERNEL_VERSION="%%KERNEL_VERSION%%"
+
+# The install script is expected to output the path of the directory
+# to use for bazel builds.
+install_bazel_build() {
+
+    build="${KERNEL_VERSION}/build"
+    if [ -d "$build" ] ; then
+        echo "$KERNEL_VERSION"
+        exit 0
+    fi
+
+    echo "ERROR: build directory not detected - installation failed?" 1>&2
+    echo "ERROR: was looking for $build in $PWD" 1>&2
+    exit 1
+}
+
+install_bazel_build

kbuild/v2/.gitignore
@@ -1,1 +1,0 @@
-local-config.sh

kbuild/v2/build.sh
@@ -1,63 +1,0 @@
-#!/bin/sh
-
-# This script builds the .debs for an Enfabrica kernel
-#
-# Inputs:
-# - a build directory
-# - a kernel branch, e.g. enf/impish-19.19
-# - a space delimited list of kernel flavours, e.g. "minimal generic"
-# - an output directory to place the generated .debs
-
-set -e
-
-BUILD_DIR="$(realpath $1)"
-KERNEL_BRANCH="$2"
-KERNEL_FLAVOURS="$3"
-OUTPUT_DEB_DIR="$(realpath $4)"
-
-if [ -z "$BUILD_DIR" ] ; then
-    echo "ERROR: build directory is not defined"
-    exit 1
-fi
-
-if [ -z "$KERNEL_BRANCH" ] ; then
-    echo "ERROR: kernel branch not specified"
-    exit 1
-fi
-
-if [ -z "$KERNEL_FLAVOURS" ] ; then
-    echo "ERROR: kernel flavours not specified: valid values 'minimal generic'"
-    exit 1
-fi
-
-KERNEL_DIR="${BUILD_DIR}/enf-linux"
-
-# clean build dir
-mkdir -p $BUILD_DIR
-rm -f "${BUILD_DIR}/linux-"*deb || true
-
-# clean output .deb dir
-rm -rf "$OUTPUT_DEB_DIR"
-mkdir -p "$OUTPUT_DEB_DIR"
-
-# Get latest kernel tree
-if [ ! -d "$KERNEL_DIR" ] ; then
-    git clone git@github.com:enfabrica/linux.git "$KERNEL_DIR"
-else
-    cd $KERNEL_DIR
-    git fetch origin
-    cd - > /dev/null 2>&1
-fi
-
-# Check out latest branch
-cd $KERNEL_DIR
-git checkout main
-git branch -D $KERNEL_BRANCH > /dev/null 2>&1 || true
-git checkout $KERNEL_BRANCH
-git clean -dfx
-
-# build all the flavours
-./enfabrica/build-kernel.sh -b auto -f "$KERNEL_FLAVOURS"
-
-# copy out the resulting .debs
-cp -av ../linux-*deb "$OUTPUT_DEB_DIR"

kbuild/v2/kpub-astore
@@ -1,0 +1,209 @@
+#!/bin/sh
+
+# This script builds kernel artifacts and publishes them to astore.
+
+set -e
+
+TOOL_PATH="$(dirname $(realpath $0))"
+SCRIPT_PATH="${TOOL_PATH}/scripts"
+
+# KERNEL_REPO: enf-linux kernel repo
+DEFAULT_KERNEL_REPO="git@github.com:enfabrica/linux.git"
+
+# KERNEL_BRANCH: enf-linux kernel branch to build
+DEFAULT_KERNEL_BRANCH="enf/impish-19.19"
+
+# KERNEL_FLAVOURS: space separated list of kernel flavours to build
+DEFAULT_KERNEL_FLAVOURS="minimal generic"
+
+# BUILD_ROOT -- scratch space to perform the build
+DEFAULT_BUILD_ROOT="${HOME}/scratch/kernel-builder"
+
+# ASTORE_ROOT -- root directory in astore to store outputs
+DEFAULT_ASTORE_ROOT="home/${USER}/scratch/kernel"
+
+# CLEAN_BUILD -- whether to wipe the build directory first Set
+# CLEAN_BUILD to "no" to skip some lengthy steps during development.
+# For production this variable should be left at the default of "yes".
+DEFAULT_CLEAN_BUILD="yes"
+
+# VERBOSE -- turn on extra debug output
+DEFAULT_VERBOSE="no"
+
+# The following ENF_ variables provide an external API and can be set
+# before running this script:
+
+RT_KERNEL_REPO="${ENF_KERNEL_REPO:-${DEFAULT_KERNEL_REPO}}"
+RT_KERNEL_BRANCH="${ENF_KERNEL_BRANCH:-${DEFAULT_KERNEL_BRANCH}}"
+RT_KERNEL_FLAVOURS="${ENF_KERNEL_FLAVOURS:-${DEFAULT_KERNEL_FLAVOURS}}"
+RT_BUILD_ROOT="${ENF_BUILD_ROOT:-${DEFAULT_BUILD_ROOT}}"
+RT_ASTORE_ROOT="${ENF_ASTORE_ROOT:-${DEFAULT_ASTORE_ROOT}}"
+RT_CLEAN_BUILD="${ENF_CLEAN_BUILD:-${DEFAULT_CLEAN_BUILD}}"
+RT_VERBOSE="${ENF_VERBOSE:-${DEFAULT_VERBOSE}}"
+
+usage() {
+    cat <<EOF
+USAGE:
+    ${0##*/} [OPTIONS]
+
+OPTIONS:
+    -r kernel_repo
+
+		Kernel repository to clone.
+
+		The default is "$DEFAULT_KERNEL_REPO".
+
+    -b kernel_branch
+
+		Kernel git branch to check out and build.
+
+		The default is "$DEFAULT_KERNEL_BRANCH".
+
+    -f flavours
+
+		A space separated list of kernel flavours
+		(configurations) to build.  See
+		debian.master/rules.d/amd64.mk in the kernel
+		repo for a list of available flavours.
+
+		The default is "$DEFAULT_KERNEL_FLAVOURS".
+
+    -o output build directory
+
+		The scratch space root directory to use for building.
+
+		By default, the output directory is first removed and
+		then created.  To preserve and reuse an existing
+		output directory see the "-p" option below.
+
+		The default is "$DEFAULT_BUILD_ROOT".
+
+    -a astore root directory
+
+		The root directory within astore to publish artifacts.
+
+		The default is "$DEFAULT_ASTORE_ROOT".
+
+    -p
+
+		WARNING: FOR DEVELOPMENT ONLY
+
+		Preserve and reuse the existing output directory.
+
+		This can save some time when maintaining these
+		scripts, but generally should not be used for
+		production.
+
+    -v
+
+		Enable verbose output.
+
+ENVIRONMENT VARIABLES
+
+The above options can also be set via environment variables:
+
+ENF_KERNEL_REPO:      (current_value: ${ENF_KERNEL_REPO:-unset})
+ENF_KERNEL_BRANCH:    (current_value: ${ENF_KERNEL_BRANCH:-unset})
+ENF_KERNEL_FLAVOURS:  (current_value: ${ENF_KERNEL_FLAVOURS:-unset})
+ENF_BUILD_ROOT:	      (current_value: ${ENF_BUILD_ROOT:-unset})
+ENF_ASTORE_ROOT:      (current_value: ${ENF_ASTORE_ROOT:-unset})
+
+In all cases, the command line arguments take precedence.
+
+EOF
+}
+
+# Command line argument override any environment variables
+while getopts hvpr:b:f:o:a: opt ; do
+    case $opt in
+        r)
+            RT_KERNEL_REPO=$OPTARG
+            ;;
+        b)
+            RT_KERNEL_BRANCH=$OPTARG
+            ;;
+        f)
+            RT_KERNEL_FLAVOURS=$OPTARG
+            ;;
+        o)
+            RT_BUILD_ROOT=$OPTARG
+            ;;
+        a)
+            RT_ASTORE_ROOT=$OPTARG
+            ;;
+        p)
+            RT_CLEAN_BUILD="no"
+            ;;
+        v)
+            RT_VERBOSE="yes"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+echo "Using configuration:"
+for var in RT_KERNEL_REPO RT_KERNEL_BRANCH RT_KERNEL_FLAVOURS RT_BUILD_ROOT RT_ASTORE_ROOT RT_CLEAN_BUILD RT_VERBOSE; do
+    printf "%-20s:   %s\n" "$var" "$(eval echo -n \$$var)"
+done
+
+if [ "$RT_VERBOSE" = "yes" ] ; then
+    set -x
+    RUN="/bin/sh -x"
+fi
+
+ASTORE_BASE="${RT_ASTORE_ROOT}/${RT_KERNEL_BRANCH}"
+
+export RT_CLEAN_BUILD
+if [ "$RT_CLEAN_BUILD" = "yes" ] ; then
+    rm -rf "$RT_BUILD_ROOT"
+fi
+
+# These directories and files are intermediate build artifacts used by
+# the scripts.
+KBUILD_DIR="$RT_BUILD_ROOT/build"
+KERNEL_DIR="${KBUILD_DIR}/enf-linux"
+KERNEL_VERSION="${RT_BUILD_ROOT}/kernel-version.txt"
+OUTPUT_DEB_DIR="$RT_BUILD_ROOT/deb"
+OUTPUT_REPO_DIR="$RT_BUILD_ROOT/repo"
+OUTPUT_BAZEL_ARCHIVE_DIR="$RT_BUILD_ROOT/bazel-archive"
+OUTPUT_APT_ARCHIVE_DIR="$RT_BUILD_ROOT/deb-archive"
+
+OUTPUT_UML_DIR="$RT_BUILD_ROOT/uml"
+OUTPUT_UML_BAZEL_ARCHIVE_DIR="$RT_BUILD_ROOT/uml/bazel"
+
+mkdir -p "$KERNEL_DIR"
+
+# Initialize the build area and clone the kernel repo
+$RUN ${SCRIPT_PATH}/init-build.sh "$KERNEL_DIR" "$RT_KERNEL_REPO" "$RT_KERNEL_BRANCH" "$KERNEL_VERSION"
+
+# Builds the .deb kernel packages for all flavours
+$RUN ${SCRIPT_PATH}/build-debs.sh "$KERNEL_DIR" "$KERNEL_VERSION" "$RT_KERNEL_FLAVOURS" "$OUTPUT_DEB_DIR"
+
+# Creates a portable Debian APT repository for each flavour
+$RUN ${SCRIPT_PATH}/repo-deb.sh "$OUTPUT_DEB_DIR" "$RT_KERNEL_FLAVOURS" "$OUTPUT_REPO_DIR"
+
+# Creates a bazel ready tarball for building kernel modules
+$RUN ${SCRIPT_PATH}/archive-bazel-deb.sh "$OUTPUT_DEB_DIR" "$RT_KERNEL_FLAVOURS" "$OUTPUT_BAZEL_ARCHIVE_DIR"
+
+# Creates a tarball of a Debian APT repository for each flavour
+$RUN ${SCRIPT_PATH}/archive-deb.sh "$OUTPUT_DEB_DIR" "$OUTPUT_REPO_DIR" "$RT_KERNEL_FLAVOURS" "$OUTPUT_APT_ARCHIVE_DIR"
+
+# Uploads the bazel ready tarball for each flavour
+$RUN ${SCRIPT_PATH}/upload-deb.sh "$OUTPUT_DEB_DIR" "$OUTPUT_BAZEL_ARCHIVE_DIR" "$OUTPUT_APT_ARCHIVE_DIR" "$RT_KERNEL_FLAVOURS" "$ASTORE_BASE"
+
+# Builds the UML kernel
+$RUN ${SCRIPT_PATH}/build-uml.sh "$KERNEL_DIR" "$KERNEL_VERSION" "$OUTPUT_UML_DIR"
+
+# For UML, creates a bazel ready tarball for building kernel modules
+$RUN ${SCRIPT_PATH}/archive-bazel-uml.sh "$KERNEL_DIR" "$OUTPUT_UML_DIR" "$OUTPUT_UML_BAZEL_ARCHIVE_DIR"
+
+# For UML, uploads the bazel ready tarball and kernel image
+$RUN ${SCRIPT_PATH}/upload-uml.sh "$OUTPUT_UML_DIR" "$OUTPUT_UML_BAZEL_ARCHIVE_DIR" "$ASTORE_BASE"

kbuild/v2/sample-runner.sh
@@ -1,58 +1,0 @@
-#!/bin/sh
-
-# This is an example script that shows how to run the scripts in order.
-#
-# This script assumes it is run from this directory
-
-set -e
-
-if [ -r ./local-config.sh ] ; then
-    . ./local-config.sh
-fi
-
-# KERNEL_BRANCH: enf-linux kernel branch to build
-KERNEL_BRANCH=${KERNEL_BRANCH:-enf/impish-19.19}
-
-# KERNEL_FLAVOURS: space separated list of kernel flavours to build
-KERNEL_FLAVOURS=${KERNEL_FLAVOURS:-"minimal generic"}
-
-# BUILD_ROOT -- scratch space to perform the build
-BUILD_ROOT=${BUILD_ROOT:-${HOME}/scratch/kernel-builder}
-
-# These directories are intermediate build directories used by the
-# scripts.
-BUILD_DIR="$BUILD_ROOT/build"
-OUTPUT_DEB_DIR="$BUILD_ROOT/deb"
-OUTPUT_REPO_DIR="$BUILD_ROOT/repo"
-OUTPUT_BAZEL_ARCHIVE_DIR="$BUILD_ROOT/bazel-archive"
-OUTPUT_APT_ARCHIVE_DIR="$BUILD_ROOT/deb-archive"
-
-mkdir -p $BUILD_DIR
-
-# Builds the .deb kernel packages for all flavours
-./build.sh $BUILD_DIR $KERNEL_BRANCH "$KERNEL_FLAVOURS" $OUTPUT_DEB_DIR
-
-# Creates a portable Debian APT repository for each flavour
-./repo.sh $OUTPUT_DEB_DIR "$KERNEL_FLAVOURS" $OUTPUT_REPO_DIR
-
-# Creates a bazel ready tarball for building kernel modules
-./archive-bazel.sh $OUTPUT_DEB_DIR "$KERNEL_FLAVOURS" $OUTPUT_BAZEL_ARCHIVE_DIR
-
-# Creates a tarball of a Debian APT repository for each flavour
-./archive-deb.sh $OUTPUT_DEB_DIR $OUTPUT_REPO_DIR "$KERNEL_FLAVOURS" $OUTPUT_APT_ARCHIVE_DIR
-
-# Uploads the bazel ready tarball for each flavour
-./upload.sh $OUTPUT_DEB_DIR $OUTPUT_BAZEL_ARCHIVE_DIR $OUTPUT_APT_ARCHIVE_DIR "$KERNEL_FLAVOURS" "kernel/${KERNEL_BRANCH}"
-
-# Next update the bazel WORKSPACE with new kernel_tree_version() stanzas for each flavour
-# Should be something like:
-#
-# kernel_tree_version(
-#     name = "enf-ubuntu-impish-minimal",
-#     package = "enf-5.13.0-19-1-1638665569-ga3b7c71b84a3-minimal",
-#     sha256 = "157811f836bf80fdefc3fd70a9b86f7b906866cbaf23a95c1fd8245bd99ffaef",
-#     url = astore_url(
-#         package = "/kernel/enf/impish-19.19/minimal/kernel-artifacts.tar.gz",
-#         uid = "ygwiif3nhniqjmqiugr6gnbj3746jtns",
-#     ),
-# )

kbuild/v2/scripts/archive-bazel-deb.sh
@@ -16,7 +16,7 @@ INPUT_DEB_ROOT="$(realpath $1)"
 KERNEL_FLAVOURS="$2"
 OUTPUT_ARCHIVE_ROOT="$(realpath $3)"
 
-INSTALL_TEMPLATE="$(dirname $(realpath $0))/template/install-bazel.sh"
+INSTALL_TEMPLATE="$(dirname $(realpath $0))/../template/install-bazel.sh"
 if [ ! -r "$INSTALL_TEMPLATE" ] ; then
     echo "ERROR: unable to find bazel install script template: $INSTALL_TEMPLATE"
     exit 1

kbuild/v2/scripts/archive-bazel-uml.sh
@@ -1,0 +1,75 @@
+#!/bin/sh
+
+# This script generates a bazel tarball for building UML kernel modules
+#
+# Inputs:
+# - the kernel source directory
+# - the kernel build directory
+# - the UML bazel archive output directory
+
+set -e
+
+KERNEL_DIR="$(realpath $1)"
+OUTPUT_UML_DIR="$(realpath $2)"
+OUTPUT_UML_BAZEL_ARCHIVE_DIR="$(realpath $3)"
+
+if [ ! -d "$KERNEL_DIR" ] ; then
+    echo "ERROR: kernel build directory does not exist: $KERNEL_DIR"
+    exit 1
+fi
+
+if [ ! -d "$OUTPUT_UML_DIR" ] ; then
+    echo "ERROR: UML kernel build directory does not exist: $OUTPUT_UML_DIR"
+    exit 1
+fi
+
+INSTALL_TEMPLATE="$(dirname $(realpath $0))/../template/install-uml-bazel.sh"
+if [ ! -r "$INSTALL_TEMPLATE" ] ; then
+    echo "ERROR: unable to find bazel UML install script template: $INSTALL_TEMPLATE"
+    exit 1
+fi
+
+KERNEL_VERSION=$(cat "${OUTPUT_UML_DIR}/include/config/kernel.release")
+
+# clean output UML bazel archive dir
+rm -rf "$OUTPUT_UML_BAZEL_ARCHIVE_DIR"
+mkdir -p "$OUTPUT_UML_BAZEL_ARCHIVE_DIR"
+
+archive="${OUTPUT_UML_BAZEL_ARCHIVE_DIR}/build-headers.tar.gz"
+
+tmp_dir="${OUTPUT_UML_BAZEL_ARCHIVE_DIR}/tmp"
+target_dir="${tmp_dir}/${KERNEL_VERSION}/build"
+mkdir -p "$target_dir"
+
+# add common files and directories to archive
+for f in scripts tools arch include Makefile ; do
+    cp -a "${OUTPUT_UML_DIR}/source/$f" $target_dir
+done
+
+# add build specific directories to archive
+for f in scripts arch include ; do
+    rsync -a "${OUTPUT_UML_DIR}/${f}/" "${target_dir}/$f"
+done
+
+# add build specific files to archive
+for f in .config Module.symvers ; do
+    rsync -a "${OUTPUT_UML_DIR}/${f}" "${target_dir}"
+done
+
+# create the bazel install script from a template
+install_script="${tmp_dir}/install-${KERNEL_VERSION}.sh"
+rm -f "$install_script"
+cp  "$INSTALL_TEMPLATE" "$install_script"
+
+# substitute some vars into the script template
+for var in KERNEL_VERSION ; do
+    sed -i -e "s/%%${var}%%/$(eval echo -n \$$var)/" $install_script
+done
+chmod 755 "$install_script"
+
+# create tarball
+echo -n "UML: Generating bazel build archive file... "
+tar -C "$tmp_dir" --create --gzip --file "$archive" .
+rm -rf "$tmp_dir"
+
+echo "Done."

kbuild/v2/scripts/archive-deb.sh
@@ -18,7 +18,7 @@ INPUT_REPO_ROOT="$(realpath $2)"
 KERNEL_FLAVOURS="$3"
 OUTPUT_ARCHIVE_ROOT="$(realpath $4)"
 
-INSTALL_TEMPLATE="$(dirname $(realpath $0))/template/install-deb.sh"
+INSTALL_TEMPLATE="$(dirname $(realpath $0))/../template/install-deb.sh"
 if [ ! -r "$INSTALL_TEMPLATE" ] ; then
     echo "ERROR: unable to find install script template: $INSTALL_TEMPLATE"
     exit 1

commit 0d06f6d16827058ab4dc5adcd8962c59e49ea46a

kbuild:uml: update build-header installer to match build rules
The UML kernel build-header install script did not install things
correctly.  The underlying bazel rule creates a filegroup() using:

  srcs = glob(["lib", "usr", "install"], allow_empty = True, exclude_directories = 0)

The previous installer was not using one these directories at
installer, hence the files were not available to UML kernel module
builds.

This patch updates the installer to use the "install" directory.

Fixes: dbf665348d4f ("kbuild/v2: Update scripts to build/publish UML kernel artifacts (#577)")

Signed-off-by: Curt Brune <curt@enfabrica.net>

kbuild/v2/template/install-uml-bazel.sh
@@ -13,7 +13,10 @@ install_bazel_build() {
 
     build="${KERNEL_VERSION}/build"
     if [ -d "$build" ] ; then
-        echo "$KERNEL_VERSION"
+        # The bazel consumer of this script will only export the
+        # following directories: ["lib", "usr", "install"]
+        mv "$KERNEL_VERSION" install
+        echo "install"
         exit 0
     fi
 