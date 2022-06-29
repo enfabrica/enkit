887fffb728e6baabca63cd51e6ead16529c7d89f
Scott Minor <scott@enfabrica.net>
2022-06-29 21:16:23 +0000

    enkit: Add version subcommand
    This change adds a `version` subcommand to enkit that prints linkstamped
    info so that we can more easily debug issues that arise from an
    out-of-date enkit version.
    
    Tested: `bazel run //enkit -- version`

---
+++ enkit/version/version.go
@@ -0,0 +1,42 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/enfabrica/enkit/lib/client"
+	"github.com/enfabrica/enkit/lib/stamp"
+
+	"github.com/spf13/cobra"
+)
+
+type Root struct {
+	*cobra.Command
+	*client.BaseFlags
+}
+
+func New(base *client.BaseFlags) *Root {
+	rc := &Root{
+		Command: &cobra.Command{
+			Use:           "version",
+			Short:         "Show version info",
+			SilenceUsage:  true,
+			SilenceErrors: true,
+			Long:          "version - command for getting the version of this tool",
+		},
+		BaseFlags: base,
+	}
+	rc.Command.RunE = rc.Run
+	return rc
+}
+
+func (r *Root) Run(cmd *cobra.Command, args []string) error {
+	fmt.Printf("Built from commit: %s\n", stamp.GitSha)
+	if stamp.GitBranch != "master" {
+		fmt.Printf("Branch is based on master commit %s\n", stamp.GitMasterSha)
+	}
+	fmt.Printf("Built from branch: %s\n", stamp.GitBranch)
+	fmt.Printf("Builder: %s\n", stamp.BuildUser)
+	fmt.Printf("Clean build: %v\n", stamp.IsClean())
+	fmt.Printf("Official build: %v\n", stamp.IsOfficial())
+	return nil
+}
