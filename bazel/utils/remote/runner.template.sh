e0a7e2c7e33bcb54c72339a63909c05f6b7cbdf1
Carlo Contavalli <carlo@enfabrica.net>
2022-07-06 17:49:36 +0000

    bazel/utils/remote: allow local copy, allow non-exec targets.
    With this PR, remote_run is renamed to export_and_run, and can now:
    - export a target to a local directory. This will copy the target
      and all its necessary dependencies. This is useful for example
      when there are generated files involved, and you want to use a
      non bazel build or tooling.
    - export (or remote copy) a non-executable target. When a target
      is not executable, files are just copied.
    - allow for detinations that contain a full path. So to copy files
      in a specific remote directory, one can use 'machine:/path/to/dir'.
    
    Added corresponding tests.

--- bazel/utils/remote/runner.template.sh
+++ bazel/utils/remote/runner.template.sh
@@ -3,8 +3,9 @@
 RSYNC_OPTS={rsync_opts}
 SSH_OPTS={ssh_opts}
 TARGET_OPTS={target_opts}
-MACHINES={machines}
+DESTS={dests}
 ONLY_COPY={only_copy}
+NO_EXECUTE={no_execute}
 SSH_CMD={ssh_cmd}
 RSYNC_CMD={rsync_cmd}
 
@@ -14,23 +15,37 @@ help() {
   } 
 
   cat <<END
-This script runs a target on a remote machine, after copying all its dependencies.
+This script runs a target from a specified destination, after copying all its dependencies.
 
 Use as:
 
-  bazel run $TARGET -- [-r rsync options]... [-s ssh options]... [-t target options]... [-o|-h] [MACHINE]...
+  bazel run $TARGET -- [-r rsync options]... [-s ssh options]... [-t target options]... [-o|-h] [DEST]...
 
 Accepted options:
 
-  MACHINE      List of machines to copy the target to. The target will then be
-               run on the first machine specified, unless -o is used.
+  DEST         List of dests to copy the target to. The target will then be
+               run on the first dest specified, unless -o is used.
 
-               The list of machines is MANDATORY, unless it is already specified
+               The list of dests is MANDATORY, unless it is already specified
                in the rule in the BUILD.bazel file. In that case, if you pass any
-               machine on the command line the list will REPLACE the one supplied
+               dest on the command line the list will REPLACE the one supplied
                in the BUILD.bazel file.
 
-  -o           Don't run the binary, only copy all the files to the remote machines.
+               DEST can be:
+                 1) A string without / or :, assumed to be a machine name.
+		    In this case, ssh will be used to run the remote command,
+                    and the defined destination directory will be appended.
+                 2) A string containing / or :, assumed to be a full path
+                    specified by the user. No path will be appended. ssh will
+                    be used if there's a ':' in the name.
+
+                 This roughly matches rsync and scp syntax: a string like
+                 'machine00.corp' will be turned into 'machine00.corp:dest/path/',
+                 and use ssh. A string like 'machine00.corp:whatever' is assumed
+                 to already contain a path, and will not be mangled. A string like
+                 './tests' will be used as a plain destination directory.
+
+  -o           Don't run the binary, only copy all the files to the remote dests.
 
   -r [value]   Adds one or more rsync options.
 
@@ -43,6 +58,11 @@ Accepted options:
      option. Those options are highly dependant on the target.
 
   -h           Prints this astonishingly helpful message.
+
+****************************************************************************************
+IMPORTANT/DANGER/ACHTUNG: rsync will overwrite and delete files in the output directory.
+         Make sure to never point this script to your home, or to a populated directory.
+****************************************************************************************
 END
 
   test -z "$*" || {
@@ -66,11 +86,11 @@ done
 shift $((OPTIND - 1))
 
 [ "$#" -le 0 ] || {
-  MACHINES=("$@")
+  DESTS=("$@")
 }
 
-[ "${#MACHINES[@]}" -ge 1 ] || {
-  help "You must specify one or more MACHINE to copy the output to"
+[ "${#DESTS[@]}" -ge 1 ] || {
+  help "You must specify one or more DEST to copy the output to"
 }
 
 target={target}
@@ -81,15 +101,31 @@ executable={executable}
 workspace={workspace}
 
 printrun() { echo "+ $*"; "$@"; }
+is_fullpath() { [[ "$1" =~ [/:] ]] || return 1; return 0; }
+is_remote() {
+  if [[ "$1" =~ [:] ]] || ! [[ "$1" =~ [/:] ]]; then 
+    return 0
+  fi
+  return 1 
+}
 
 echo "Copying files..."
 set -e
-for machine in "${MACHINES[@]}"; do
-  printrun $RSYNC_CMD --files-from="$include" "${RSYNC_OPTS[@]}" .. "$machine:$destpath/"
+destrun="" # Actual location where the test will be run from.
+for dest in "${DESTS[@]}"; do
+  # Machine already specifies a path, or it's a local directory.
+  # Don't mess with the path supplied by the user.
+  if is_fullpath "$dest"; then
+    printrun $RSYNC_CMD --files-from="$include" "${RSYNC_OPTS[@]}" .. "$dest"
+    test -n "$destrun" || destrun="$dest/$workspace"
+  else
+    printrun $RSYNC_CMD --files-from="$include" "${RSYNC_OPTS[@]}" .. "$dest:$destpath/"
+    test -n "$destrun" || destrun="$destpath/$workspace"
+  fi
 done
 
 # TODO(cccontavalli): better escaping, will fix it once we have more tests.
-command="cd $destpath/$workspace; MACHINES='${MACHINES[*]}' ./$executable ${TARGET_OPTS[*]}"
+command="cd $destrun; MACHINES='${DESTS[*]}' ./$executable ${TARGET_OPTS[*]}"
 [ "$ONLY_COPY" != "true" ] || {
   echo "Copy only mode was requesting - not running any command"
   echo "Would have run:"
@@ -97,5 +133,15 @@ command="cd $destpath/$workspace; MACHINES='${MACHINES[*]}' ./$executable ${TARG
   exit 0
 }
 
-echo "Running '$command' on ${MACHINES[0]}..."
-printrun $SSH_CMD "${SSH_OPTS[@]}" "${MACHINES[0]}" -- "$command"
+[ "$NO_EXECUTE" != "true" ] || {
+  echo "Target $target is not executable - only copied"
+  exit 0
+}
+
+target="${DESTS[0]}"
+echo "Running '$command' on $target..."
+if is_remote "$target"; then
+  printrun $SSH_CMD "${SSH_OPTS[@]}" "$target" -- "$command"
+else
+  printrun "$command"
+fi
