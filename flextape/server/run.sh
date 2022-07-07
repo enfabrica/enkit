37376b67e78b050c796446b8028ecfbf9952ebd3
rolf <rolf@enfabrica.net>
2022-07-07 00:30:30 +0000

    updated flextape innovus counts

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:a3af8d3ddf84d666a064eb4826d2aa8bb8dba42c9ddc1734192a0c8523f64ad4"
+  "gcr.io/devops-284019/infra/flextape@sha256:0c4c11fc5d74f74925ea240471cdcb0b1a592d985ccc0d7094bbdf62e5460eb0"
