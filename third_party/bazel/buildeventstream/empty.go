40d72070e1bf878202b19dea184cb2f221be9bb0
Scott Minor <scott@enfabrica.net>
2022-07-09 05:14:01 +0000

    Make repo `go get`-clean
    This change employs a couple tricks so that `go get -u ./...` can
    successfully update `go.mod` and `go.sum`.
    
    * For packages containing only proto-generated code, an empty source
      file is created to satisfy `go get`.
    * For packages containing only go-embed generated code, the BUILD rules
      are moved to a BUILD file that reflects the import path, and an empty
      source file is created to satisfy `go get`.
    
    The resulting changes are tested to be gazelle-clean; in some cases,
    directives were added or left intact to prevent gazelle from
    overzealously cleaning up rules. These directives can in some cases be
    removed once rules_go is updated and we're using a non-deprecated way of
    doing go embedding.
    
    Tested:
    * `bazel test //...` succeeds
    * `bazel run //:gazelle` causes no diffs

---
+++ third_party/bazel/buildeventstream/empty.go
@@ -0,0 +1,4 @@
+// Package buildeventstream is actually satisfied by generated code from
+// //third_party/bazel/src/main/java/com/google/devtools/build/lib/buildeventstream/proto;
+// this empty package helps satisfy `go get -u ./...`.
+package buildeventstream
