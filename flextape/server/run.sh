dc573a108e821c3d040a6012c8eca08df2cfb876
rolf <rolf@enfabrica.net>
2022-10-20 04:12:21 +0000

    update with latest counts

--- flextape/server/run.sh
+++ flextape/server/run.sh
@@ -9,4 +9,4 @@ docker run \
   -e "PORT=${PORT}" \
   --name=flextape \
   --restart="always" \
-  "gcr.io/devops-284019/infra/flextape@sha256:9fc099c1aefa0465855672d34d40e54cfb55275e465a130f884bfccd3e9eae1b"
+  "gcr.io/devops-284019/infra/flextape@sha256:5e89309c81ca9e6d173dd56de13fb76c51ec80e77371886f97ccaedb87df4f6b"
