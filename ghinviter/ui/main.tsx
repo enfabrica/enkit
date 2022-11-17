aac7d7c6f374d4cfc1abb2da16a355cf9cb1417b
Carlo Contavalli <carlo@enfabrica.net>
2022-11-17 06:23:15 +0000

    basic material-ui integration is working.

--- ghinviter/ui/main.tsx
+++ ghinviter/ui/main.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+
+import Stepper from './stepper';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
-  return <div>Hello world, my darling</div>;
+  return (
+    <React.Fragment>
+        <CssBaseline enableColorScheme />
+        <Grid container spacing={2}>
+          <Grid xs={12} sm={6} smOffset={3}>
+            <Paper>
+              Hello world, my darling
+              <Stepper />
+            </Paper>
+          </Grid>
+        </Grid>
+    </React.Fragment>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
