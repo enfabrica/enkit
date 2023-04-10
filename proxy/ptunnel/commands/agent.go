95eb0e6e132b0f11f6f345e1e8a5a126aa547161
Carlo Contavalli <carlo@enfabrica.net>
2023-04-10 16:15:16 +0000

    proxy/ptunnel/commands/agent: drop deprecated string, noop.
    Just removes a global variable that's no longer used anywhere.
    (Code was modified to use printf with no template instead)

--- proxy/ptunnel/commands/agent.go
+++ proxy/ptunnel/commands/agent.go
@@ -88,11 +88,6 @@ func RunAgentCommand(command *cobra.Command, bf *client.BaseFlags, args []string
 	return nil
 }
 
-const PrintSSHTemplate = `SSH_AUTH_SOCK=%s; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=%d; export SSH_AGENT_PID;
-echo Agent pid %d;
-`
-
 func NewPrintCommand(parent *cobra.Command, bf *client.BaseFlags) *cobra.Command {
 	c := &cobra.Command{
 		Use:   "print",
