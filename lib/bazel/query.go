e3597fe5a0096be6a8c18c74b1d34dc3d823c5d3
Scott Minor <scott@enfabrica.net>
2023-12-14 01:08:34 +0000

    `enkit bazel affected-targets`: Improve memory usage
    This change reduces the memory usage of `enkit bazel affected-targets
    list` for large build graphs.
    
    Prior to this change, returned proto Target objects were parsed and
    wrapped with our own struct to add additional fields. Hashes were then
    calculated lazily once all the objects were loaded into memory. Upon
    inspection, it appeared that the bulk of memory usage used by the
    unmarshaled proto objects.
    
    This change tries to avoid keeping too much of the proto objects
    in-memory by pulling them out of our "wrapper" (now no longer a wrapper)
    and calculating a "shallow hash" up-front. The shallow hash will capture
    any changes in the target's attributes or source file contents, so that
    all of that input data can be discarded for the rest of execution. The
    target object now also stores additional attributes that calling code
    needs to reference later.
    
    Construction of targets in external workspaces is changed: instead of
    following a singular constructor and then calling a mutating function
    later to fix them up, a second constructor is introduced.
    
    Some tests are no longer relevant and are deleted.
    
    Tested:
    * unit tests pass
    * ran manually:
      - diffed between HEAD~1 and HEAD, and between HEAD~1001 and HEAD~1000
      - new max RSS seen is ~1GiB, down from ~6GiB
      - number of detected changed targets is consistent between new and old
    
    Jira: INFRA-9036

--- lib/bazel/query.go
+++ lib/bazel/query.go
@@ -20,16 +20,7 @@ type QueryResult struct {
 }
 
 func (r *QueryResult) TargetHashes() (TargetHashes, error) {
-	// Add a single attribute to each external target with the sorted SHA256 sums
-	// of all their downloads. This will allow for the subsequent hashing to
-	// change the hash for the entire coarse external target if any of the
-	// download SHA256 sums change.
-	err := r.addChecksumsAttributeToExternals()
-	if err != nil {
-		return nil, fmt.Errorf("failed to add checksums to external targets: %w", err)
-	}
-
-	err = fillDependencies(r.Targets)
+	err := fillDependencies(r.Targets)
 	if err != nil {
 		return nil, fmt.Errorf("failed to link dependencies: %w", err)
 	}
@@ -47,95 +38,8 @@ func (r *QueryResult) TargetHashes() (TargetHashes, error) {
 
 func fillDependencies(targets map[string]*Target) error {
 	for _, t := range targets {
-		switch t.Target.GetType() {
-		case bpb.Target_RULE:
-			deps := t.Target.GetRule().GetRuleInput()
-			for _, dep := range deps {
-				depTarget, ok := targets[dep]
-				if !ok {
-					// TODO(scott): Log this condition
-					return fmt.Errorf("dep %q not found for target %q", dep, t.Name())
-					continue
-				}
-				t.deps = append(t.deps, depTarget)
-			}
-		case bpb.Target_GENERATED_FILE:
-			dep := t.Target.GetGeneratedFile().GetGeneratingRule()
-			depTarget, ok := targets[dep]
-			if !ok {
-				// TODO(scott): Log this condition
-				return fmt.Errorf("dep %q not found for target %q", dep, t.Name())
-				continue
-			}
-			t.deps = append(t.deps, depTarget)
-		default:
-			continue
-		}
-	}
-	return nil
-}
-
-func (r *QueryResult) addChecksumsAttributeToExternals() error {
-	for targetName, target := range r.Targets {
-		lbl, err := labelFromString(targetName)
-		if err != nil {
-			return err
-		}
-		if !lbl.isExternal() {
-			continue
-		}
-
-		lbl = lbl.toCoarseExternal()
-		events := r.WorkspaceEvents[lbl.String()]
-
-		var checksums []string
-		for _, event := range events {
-			switch e := event.GetEvent().(type) {
-			case *bpb.WorkspaceEvent_DownloadEvent:
-				if e.DownloadEvent.GetSha256() != "" {
-					checksums = append(checksums, e.DownloadEvent.GetSha256())
-				}
-				if e.DownloadEvent.GetIntegrity() != "" {
-					checksums = append(checksums, e.DownloadEvent.GetIntegrity())
-				}
-			case *bpb.WorkspaceEvent_DownloadAndExtractEvent:
-				if e.DownloadAndExtractEvent.GetSha256() != "" {
-					checksums = append(checksums, e.DownloadAndExtractEvent.GetSha256())
-				}
-				if e.DownloadAndExtractEvent.GetIntegrity() != "" {
-					checksums = append(checksums, e.DownloadAndExtractEvent.GetIntegrity())
-				}
-			case *bpb.WorkspaceEvent_ExecuteEvent:
-				if len(e.ExecuteEvent.GetArguments()) == 2 && e.ExecuteEvent.GetArguments()[0] == "echo" {
-					fmt.Printf("got checksum: %q\n", e.ExecuteEvent.GetArguments()[1])
-					checksums = append(checksums, e.ExecuteEvent.GetArguments()[1])
-				}
-			}
-		}
-
-		var deps []string
-		if target.GetType() == bpb.Target_RULE {
-			deps = target.GetRule().GetRuleInput()
-		}
-
-		// Rewrite the target as a "rule" that only has an attribute that
-		// corresponds to the checksum(s) used when downloading the repo in which it
-		// resides. Therefore, this target's hash will change iff one or more
-		// downloads for its repo has a checksum change.
-		nameCopy := targetName
-		*target.Target = bpb.Target{
-			Type: bpb.Target_RULE.Enum(),
-			Rule: &bpb.Rule{
-				Name:      &nameCopy,
-				RuleInput: deps,
-				Attribute: []*bpb.Attribute{
-					&bpb.Attribute{
-						Name:            proto.String("workspace_download_checksums"),
-						Type:            bpb.Attribute_STRING_LIST.Enum(),
-						StringListValue: checksums,
-					},
-				},
-			},
+		if err := t.ResolveDeps(targets); err != nil {
+			return fmt.Errorf("failed to fill deps for target %q: %w", t.Name(), err)
 		}
 	}
 	return nil
@@ -168,19 +72,6 @@ func (w *Workspace) Query(query string, options ...QueryOption) (*QueryResult, e
 	defer stdout.Close()
 	rdr := delimited.NewReader(stdout)
 
-	var buf []byte
-	for buf, err = rdr.Next(); err == nil; buf, err = rdr.Next() {
-		var target bpb.Target
-		if err := proto.Unmarshal(buf, &target); err != nil {
-			return nil, fmt.Errorf("failed to unmarshal Target message: %w", err)
-		}
-		newTarget := &Target{Target: &target}
-		targets[newTarget.Name()] = newTarget
-	}
-	if err != io.EOF {
-		return nil, fmt.Errorf("error while reading stdout from bazel command: %w", err)
-	}
-
 	workspaceEvents := map[string][]*bpb.WorkspaceEvent{}
 	if queryOpts.workspaceLog != nil {
 		rdr := delimited.NewReader(queryOpts.workspaceLog)
@@ -192,6 +83,34 @@ func (w *Workspace) Query(query string, options ...QueryOption) (*QueryResult, e
 			}
 			workspaceEvents[event.GetRule()] = append(workspaceEvents[event.GetRule()], &event)
 		}
+	}
+
+	var buf []byte
+	for buf, err = rdr.Next(); err == nil; buf, err = rdr.Next() {
+		var target bpb.Target
+		if err := proto.Unmarshal(buf, &target); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal Target message: %w", err)
+		}
+		lbl, err := labelFromString(extractName(&target))
+		if err != nil {
+			return nil, err
+		}
+		if lbl.isExternal() {
+			newTarget, err := NewExternalPseudoTarget(&target, workspaceEvents)
+			if err != nil {
+				return nil, err
+			}
+			targets[newTarget.Name()] = newTarget
+		} else {
+			newTarget, err := NewTarget(w, &target)
+			if err != nil {
+				return nil, err
+			}
+			targets[newTarget.Name()] = newTarget
+		}
+	}
+	if err != io.EOF {
+		return nil, fmt.Errorf("error while reading stdout from bazel command: %w", err)
 	}
 
 	return &QueryResult{
