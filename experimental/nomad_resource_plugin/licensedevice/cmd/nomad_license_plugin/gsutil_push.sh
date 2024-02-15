612ba48653270d520e2efd180f1665d82a4e7564
Jason R. Pascucci <jrp@enfabrica.net>
2024-02-15 16:43:29 +0000

    Fix shell scripts to use arguments.

--- experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/gsutil_push.sh
+++ experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/gsutil_push.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pwd
-echo relative path
-gsutil cp experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/nomad_license_plugin_/nomad_license_plugin gs://enfabrica-cluster-prod-nomad-plugin/nomad_license_plugin
+gsutil cp $1 gs://enfabrica-cluster-prod-nomad-plugin/nomad_license_plugin
 gsutil stat gs://enfabrica-cluster-prod-nomad-plugin/nomad_license_plugin
 
--- experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/gsutil_stat.sh
+++ experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/gsutil_stat.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-pwd
-echo relative path
-ls experimental/nomad_resource_plugin/licensedevice/cmd/nomad_license_plugin/nomad_license_plugin_/nomad_license_plugin
 gsutil stat gs://enfabrica-cluster-prod-nomad-plugin/nomad_license_plugin
 
