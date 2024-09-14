411d96b3ec1080d3a618d9a02621f6010a48e903
Roman Ivanov <roman@enfabrica.net>
2024-09-13 17:05:08 -0700

    add cpu to machine

--- allocation_manager/python/test_one.py
+++ allocation_manager/python/test_one.py
@@ -10,11 +10,29 @@ class Resource:
     def __init__(self, name: str = '') -> None:
         self.state = State.FREE
         self.name = name or 'randomize name her'
+        self.child = []
+    
+    def having(self, cls, count: int = 1):
+        for i in range(count):
+            self.child.append(cls())
+
+        return self
 
 
 class Machine(Resource):
     def __init__(self, name: str) -> None:
         super().__init__(name)
+
+
+class CPU(Resource):
+    """cpu resource
+    """
+    def __init__(self) -> None:
+        """cpu resource
+
+        TODO: named cpu for the case when CPUs are assigned and not shared
+        """
+        super().__init__()
 
 
 class Pool:
@@ -44,9 +62,9 @@ class Pool:
 
         return False
 
-    def allocate(self, kindof) -> Resource:
+    def allocate(self, kind_of) -> Resource:
         for each in self.pool:
-            if each.state == State.FREE and isinstance(each, kindof):
+            if each.state == State.FREE and isinstance(each, kind_of):
                 each.state = State.RESERVED
                 return each
 
@@ -71,6 +89,7 @@ def test_simple_resource_abc():
     c = pool.allocate(Resource)
     assert c is None
 
+
 def test_machine():
     pool = Pool([Resource(), Machine('server2'), Machine('server1')])
 
@@ -81,6 +100,30 @@ def test_machine():
 
     b = pool.allocate(Machine)
     assert isinstance(b, Machine)
+    print(b)
+
+    c = pool.allocate(Machine)
+    assert isinstance(c, Machine)
+    print(c)
+
+    d = pool.allocate(Machine)
+    assert d is None
+
+
+def test_machine_with_cpu():
+    pool = Pool([Resource(),
+                 Machine('server2').having(CPU, 4),
+                 Machine('server1')])
+
+    a = pool.allocate(Resource)
+    assert isinstance(a, Resource)
+    assert not isinstance(a, Machine)
+    assert len(a.child) in [0, 4]
+    print(a)
+
+    b = pool.allocate(Machine)
+    assert isinstance(b, Machine)
+    assert len(b.child) in [0, 4]
     print(b)
 
     c = pool.allocate(Machine)
