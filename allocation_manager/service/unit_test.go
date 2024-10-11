347b69cf8affca58b71fa84470a3ca67fad19d6c
Roman Ivanov <me@roivanov.com>
2024-10-11 12:54:37 -0700

    remove worthless test
    I couldn't find any `Get()` method of the `Counter`
    https://github.com/prometheus/client_golang/blob/v1.20.4/prometheus/counter.go#L35

--- allocation_manager/service/unit_test.go
+++ allocation_manager/service/unit_test.go
@@ -142,29 +142,4 @@ func TestForget(t *testing.T) {
 	assert.Equal(t, 0, u.Forget("idX"), "u.Forget second try")
 }
 
-func TestNew(t *testing.T) {
-	u := unitA()
-	assert.Equal(t, u.Topology.Name, "Unit Name")
-
-	topo := apb.Topology{
-		Name: "Unit Name 2", Config: "Unit Config",
-	}
-	a := NewUnit(topo)
-	assert.Equal(t, a.Topology.Name, "Unit Name 2")
-	_, inv1 := invX()
-	a.Allocate(inv1)
-	a.Forget(inv1.ID)
-
-	_, inv2 := invX()
-	u.Allocate(inv2)
-
-	_, inv3 := invX()
-	a.Allocate(inv3)
-	a.Forget(inv3.ID)
-
-	// TODO(roivanov): add prometheus metrics checks
-	// fmt.Printf("metrics:", a.Metrics)
-	// assert.True(t, false)
-}
-
 // TODO: test u.GetStats
