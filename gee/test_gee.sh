4c68928e11b50e9bb3b4b3955a9589d31c3107ac
Jonathan Mayer <jonathan@enfabrica.net>
2025-03-21 18:48:31 -0700

    implement rupdate

--- gee/test_gee.sh
+++ gee/test_gee.sh
@@ -36,20 +36,43 @@ else
 fi
 
 cd "${GEEDIR}/enkit/master"
-yes n | g mkbr test1
-cd "${GEEDIR}/enkit/test1"
-ls
 
-printf "1 2 3\n4 5 6\n7 8 9\n" > matrix.txt
-g commit -f -a -m "added matrix.txt"
-yes n | g mkbr test2
-printf "1 2 3\n4 X 6\n7 8 9\n" > matrix.txt
-g commit -f -a -m "matrix: changed to X"
-cd "${GEEDIR}/enkit/test2"
-printf "1 2 3\n4 Y 6\n7 8 9\n" > matrix.txt
-g commit -f -a -m "matrix: changed to Y"
-# encounter a merge conflict:
-g up
+function test_merge_conflict() {
+  echo ""
+  echo "#############################################"
+  echo "## testing merge conflict: $1"
+  echo "#############################################"
+  cd "${GEEDIR}/enkit/master"
+  if [[ ! -d "${GEEDIR}/enkit/test1" ]]; then
+    yes n | g mkbr test1
+  fi
+  cd "${GEEDIR}/enkit/test1"
+  git reset --hard master
+  printf "1 2 3\n4 5 6\n7 8 9\n" > matrix.txt
+  g commit -f -a -m "added matrix.txt"
+  if [[ ! -d "${GEEDIR}/enkit/test2" ]]; then
+    yes n | g mkbr test2
+  fi
+  cd "${GEEDIR}/enkit/test2"
+  git reset --hard test1
+  cd "${GEEDIR}/enkit/test1"
+  printf "1 2 3\n4 X 6\n7 8 9\n" > matrix.txt
+  g commit -f -a -m "matrix: changed to X"
+  cd "${GEEDIR}/enkit/test2"
+  printf "1 2 3\n4 Y 6\n7 8 9\n" > matrix.txt
+  g commit -f -a -m "matrix: changed to Y"
+  # encounter a merge conflict:
+  if [[ -n "$1" ]]; then
+    yes $1 | g up
+  else
+    g up
+  fi
+}
+
+test_merge_conflict t  # "theirs"
+test_merge_conflict y  # "yours"
+test_merge_conflict a  # "abort"
+test_merge_conflict k  # "skip"
 
 
 
