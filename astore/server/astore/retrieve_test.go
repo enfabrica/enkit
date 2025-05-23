10e6ac19d69e154f329f113c97eb3de784ea4b23
Scott Minor <scott@enfabrica.net>
2025-05-23 13:24:59 +0000

    astore: URL fetch with UID+path sets empty TagSet on Retrieve
    This change ensures that when a URL-based download is happening by UID
    that an empty TagSet gets put on the `RetrieveRequest`, to avoid the
    default behavior of a `latest` tag filter being applied.
    
    Tested: TODO
    
    Jira: REL-240

--- astore/server/astore/retrieve_test.go
+++ astore/server/astore/retrieve_test.go
@@ -2,6 +2,8 @@ package astore
 
 import (
 	"context"
+	"net/http"
+	"net/url"
 	"testing"
 
 	apb "github.com/enfabrica/enkit/astore/rpc/astore"
@@ -11,8 +13,47 @@ import (
 	"cloud.google.com/go/storage"
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	dpb "google.golang.org/genproto/googleapis/datastore/v1"
 )
+
+type testHandler struct {
+	path string
+	res  *apb.RetrieveResponse
+	err  error
+}
+
+func (h *testHandler) Download(path string, res *apb.RetrieveResponse, err error, w http.ResponseWriter, r *http.Request) {
+	h.path = path
+	h.res = res
+	h.err = err
+}
+
+type mockResponseWriter struct {
+	mock.Mock
+}
+
+func (m *mockResponseWriter) Header() http.Header {
+	args := m.Called()
+	return args.Get(0).(http.Header)
+}
+
+func (m *mockResponseWriter) Write(b []byte) (int, error) {
+	args := m.Called(b)
+	return args.Int(0), args.Error(1)
+}
+
+func (m *mockResponseWriter) WriteHeader(status int) {
+	m.Called(status)
+}
+
+func mustParseURL(t *testing.T, u string) *url.URL {
+	t.Helper()
+	parsed, err := url.Parse(u)
+	require.NoError(t, err)
+	return parsed
+}
 
 func TestServerRetrieve(t *testing.T) {
 	// ** IMPORTANT **
@@ -415,6 +456,94 @@ func TestServerRetrieve(t *testing.T) {
 			_, gotErr := srv.Retrieve(ctx, tc.req)
 			errdiff.Check(t, gotErr, tc.wantErr)
 			if gotErr != nil {
+				return
+			}
+
+			queries := ds.RecordedQueries(t)
+			assert.Equal(t, 1, len(queries))
+			testutil.AssertProtoEqual(t, queries[0], tc.wantQuery)
+		})
+	}
+}
+
+func TestServerDownloadArtifact(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		prefix    string
+		auth      authType
+		req       *http.Request
+		wantQuery *dpb.RunQueryRequest
+		wantErr   string
+	}{
+		{
+			desc:   "path and tag",
+			prefix: "/g/",
+			auth:   AuthTypeOauth,
+			req: &http.Request{
+				URL: mustParseURL(t, `https://astore.corp.enfabrica.net/g/test/package?t=1.3.1`),
+			},
+			wantQuery: runQueryRequest(&dpb.Query{
+				Filter: compositeAnd(
+					propertyEqualsString("Parent", "root/test/package"),
+					propertyEqualsString("Tag", "1.3.1"),
+					propertyHasAncestorPel("__key__", NoArch, "root", "test", "package"),
+				),
+				Kind:  kindArtifact(),
+				Limit: int32Val(1),
+				Order: []*dpb.PropertyOrder{descendingBy("Created")},
+			}),
+		},
+		{
+			desc:   "path and uid",
+			prefix: "/g/",
+			auth:   AuthTypeOauth,
+			req: &http.Request{
+				URL: mustParseURL(t, `https://astore.corp.enfabrica.net/g/test/package?u=buhi7q8isp7ttm7q3h6qnhwwzm3tjqiw`),
+			},
+			wantQuery: runQueryRequest(&dpb.Query{
+				Filter: compositeAnd(
+					propertyEqualsString("Parent", "root/test/package"),
+					propertyEqualsString("Uid", "buhi7q8isp7ttm7q3h6qnhwwzm3tjqiw"),
+					propertyHasAncestorPel("__key__", NoArch, "root", "test", "package"),
+				),
+				Kind:  kindArtifact(),
+				Limit: int32Val(1),
+				Order: []*dpb.PropertyOrder{descendingBy("Created")},
+			}),
+		},
+		{
+			desc:   "path and uid",
+			prefix: "/g/",
+			auth:   AuthTypeOauth,
+			req: &http.Request{
+				URL: mustParseURL(t, `https://astore.corp.enfabrica.net/g/test/package`),
+			},
+			wantQuery: runQueryRequest(&dpb.Query{
+				Filter: compositeAnd(
+					propertyEqualsString("Parent", "root/test/package"),
+					propertyEqualsString("Tag", "latest"),
+					propertyHasAncestorPel("__key__", NoArch, "root", "test", "package"),
+				),
+				Kind:  kindArtifact(),
+				Limit: int32Val(1),
+				Order: []*dpb.PropertyOrder{descendingBy("Created")},
+			}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			stubs := gostub.Stub(&storageSignedURL, func(string, string, *storage.SignedURLOptions) (string, error) {
+				return "https://example.com/signedurl", nil
+			})
+			defer stubs.Reset()
+
+			srv, ds := serverForTest()
+
+			th := &testHandler{}
+
+			srv.DownloadArtifact(tc.prefix, th.Download, tc.auth, &mockResponseWriter{}, tc.req)
+			errdiff.Check(t, th.err, tc.wantErr)
+			if th.err != nil {
 				return
 			}
 
