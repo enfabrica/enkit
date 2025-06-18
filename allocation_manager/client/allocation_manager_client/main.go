fff562f076d486b213d7d86014707f657637d1f8
Paul Witt <paul@enfabrica.net>
2025-06-18 20:56:27 +0000

    fix client build issues

--- allocation_manager/client/allocation_manager_client/main.go
+++ allocation_manager/client/allocation_manager_client/main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/signal"
 	"os/user"
-	"strings"
 	"syscall"
 	"time"
 
@@ -20,6 +19,7 @@ import (
 )
 
 var (
+	topology_name = flag.String("topology_name", "", "Specify a specific, known topology you wish to allocate")
 	timeout = flag.Duration("timeout", 7200*time.Second, "Max time waiting in queue")
 	purpose = flag.String("purpose", "", "What this reservation is for")
 )
@@ -30,37 +30,15 @@ func main() {
 	flag.Parse()
 	args := flag.Args()
 	if len(args) < 4 {
-		fmt.Fprintln(os.Stderr, "Usage: $0 [flags] host port config_filenames cmd [flags and args...]")
+		fmt.Fprintln(os.Stderr, "Usage: $0 [flags] host port cmd [flags and args...]")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 	host, port := args[0], args[1]
-	configFilenames := args[2]
-	cmd, args := args[3], args[4:]
+	cmd, args := args[2], args[3:]
 	user, err := user.Current()
 	if err != nil {
 		log.Fatalf("Failed to get username: %s\n", err)
-	}
-	var names []string
-	var configstrs []string
-	for _, fn := range strings.Split(configFilenames, ",") {
-		fh, err := os.Open(fn)
-		defer fh.Close()
-		if err != nil {
-			log.Fatalf("Failed to open %s: %s\n", fn, err)
-		}
-		configBytes := make([]byte, 1024000) // topology limited to 1MB
-		count, err := fh.Read(configBytes)
-		if err != nil {
-			log.Fatalf("Failed to read %s: %s\n", fn, err)
-		}
-		parsedTopology, err := topology.ParseYaml(configBytes[:count])
-		if err != nil {
-			log.Fatalf("cannot unmarshal data: %v\n", err)
-		}
-		names = append(names, parsedTopology.Name) // use the parsed yaml request for only the name.
-		fmt.Printf("Requesting unit name %s\n", parsedTopology.Name)
-		configstrs = append(configstrs, string(configBytes))
 	}
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", host, port), grpc.WithInsecure())
@@ -77,8 +55,8 @@ func main() {
 		log.Printf("Allocation Manager client caught signal %v; killing job...", sig)
 		cancel()
 	}(cancel)
-	fmt.Printf("names=%v, configstrs=%v\n", names, configstrs)
-	c := client.New(apb.NewAllocationManagerClient(conn), names, configstrs, user.Username, *purpose)
+
+	c := client.New(apb.NewAllocationManagerClient(conn), *topology_name, user.Username, *purpose)
 	err = c.Guard(ctx, cmd, args...)
 	if err != nil {
 		log.Fatal(err)
