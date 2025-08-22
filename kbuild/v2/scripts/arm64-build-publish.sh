4f7549a45f4533edaf314eb6b24479f92659649e
Roman Ivanov <roman@enfabrica.net>
2025-08-22 15:39:22 -0700

    debug

--- kbuild/v2/scripts/arm64-build-publish.sh
+++ kbuild/v2/scripts/arm64-build-publish.sh
@@ -41,7 +41,8 @@ apt update || true
 
 apt install -yV libpci-dev
 
-# export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+ls -lh /usr/lib/aarch64-linux-gnu/pkgconfig
 
 # Builds the .deb kernel packages for arch, flavour
 ${SCRIPT_PATH}/build-debs.sh "$KERNEL_SRC" "$KERNEL_VERSION" "$ARCH" "$FLAVOUR" "$BUILD_DEB_DIR" "$OUTPUT_DEB_DIR"
