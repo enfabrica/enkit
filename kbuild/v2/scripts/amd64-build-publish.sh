94273f81716d5d4221be08789d9f64e37ee96b1e
Roman Ivanov <roman@enfabrica.net>
2025-08-27 00:29:42 +0000

    list after build-debs.sh

--- kbuild/v2/scripts/amd64-build-publish.sh
+++ kbuild/v2/scripts/amd64-build-publish.sh
@@ -25,6 +25,14 @@ OUTPUT_APT_ARCHIVE_DIR="$BUILD_ROOT/deb-archive/${TARGET}"
 # Builds the .deb kernel packages for arch, flavour
 ${SCRIPT_PATH}/build-debs.sh "$KERNEL_SRC" "$KERNEL_VERSION" "$ARCH" "$FLAVOUR" "$BUILD_DEB_DIR" "$OUTPUT_DEB_DIR"
 
+find "$OUTPUT_DEB_DIR"
+touch "/workspace/${TARGET}.done"
+while [ $(ls /workspace/*done | wc -l) -lt 3 ] ; do
+    sleep 10
+done
+echo "Done with build-debs.sh"
+exit 1
+
 # Creates a portable Debian APT repository for arch, flavour
 ${SCRIPT_PATH}/repo-deb.sh "$OUTPUT_DEB_DIR" "$ARCH" "$FLAVOUR" "$OUTPUT_REPO_DIR"
 
--- kbuild/v2/scripts/arm64-build-publish.sh
+++ kbuild/v2/scripts/arm64-build-publish.sh
@@ -30,6 +30,14 @@ echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 # Builds the .deb kernel packages for arch, flavour
 ${SCRIPT_PATH}/build-debs.sh "$KERNEL_SRC" "$KERNEL_VERSION" "$ARCH" "$FLAVOUR" "$BUILD_DEB_DIR" "$OUTPUT_DEB_DIR"
 
+find "$OUTPUT_DEB_DIR"
+touch "/workspace/${TARGET}.done"
+while [ $(ls /workspace/*done | wc -l) -lt 3 ] ; do
+    sleep 10
+done
+echo "Done with build-debs.sh"
+exit 1
+
 # Creates a portable Debian APT repository for arch, flavour
 ${SCRIPT_PATH}/repo-deb.sh "$OUTPUT_DEB_DIR" "$ARCH" "$FLAVOUR" "$OUTPUT_REPO_DIR"
 
