1fc49985cbe1a57acb4db01cc18c7ab53c35d480
Roman Ivanov <roman@enfabrica.net>
2025-08-27 15:56:58 +0000

    save into file

--- kbuild/v2/scripts/arm64-build-publish.sh
+++ kbuild/v2/scripts/arm64-build-publish.sh
@@ -30,9 +30,8 @@ echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
 # Builds the .deb kernel packages for arch, flavour
 ${SCRIPT_PATH}/build-debs.sh "$KERNEL_SRC" "$KERNEL_VERSION" "$ARCH" "$FLAVOUR" "$BUILD_DEB_DIR" "$OUTPUT_DEB_DIR"
 
-find "$OUTPUT_DEB_DIR"
-touch "/workspace/${TARGET}.done"
-while [ $(ls /workspace/*done | wc -l) -lt 3 ] ; do
+find "$OUTPUT_DEB_DIR" >> /workspace/${TARGET}.done
+while [ $(ls /workspace/*done | wc -l) -lt 2 ] ; do
     sleep 10
 done
 echo "Done with build-debs.sh"
